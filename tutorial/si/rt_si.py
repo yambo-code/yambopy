83462abc1f6a720499e6273f35d82ea03042eeb0
alexmoratalla <alejandro.molina@uv.es>
2016-01-14 19:56:07 +0100

    Real time
    script for real time

--- tutorial/si/rt_si.py
+++ tutorial/si/rt_si.py
@@ -1,7 +1,24 @@
+##############################################################################
 #
 # Author: Alejandro Molina-Sanchez
 # Run real-time simulations with yambo
+# 
+# Warning: Real-time simulations requires several data folders for running
+# properly. Before using this scripts compulsively is recommended
+# to understand the different run levels.
 #
+# Intructions: 
+# The dictionary 'job' stores the variable of the calculation
+# calculation : 'collision', 'tdsex', 'negf', 'dissipation'
+# folder-col  : collision data
+# folder-run  : results (only work if collisions have been previously calculated)
+# DG          : True or False if we use the double-grid
+# nodes       : cpu-dependent variable
+# cores       :          "
+#
+# Calculations are done inside the folder FixSymm (feel free to rename it)
+#
+##############################################################################
 #from __future__ import print_function
 from yambopy.inputfile import *
 from pwpy.inputfile import *
@@ -47,97 +64,76 @@ if not os.path.isdir('FixSymm'):
   os.system('rm -r rt')
   print('FixSymm folder created')
 
-def realtime(name_folder):
-  a, b, c, d = YamboIn('%s -r -e -v c -V all'%yambo_rt,folder='FixSymm'),YamboIn('%s -q p -v c -V all'%yambo_rt,folder='FixSymm'),YamboIn('%s -q p -v c -V all'%yambo_rt,folder='FixSymm'),YamboIn('%s -s p -q p -v c -V all'%yambo_rt,folder='FixSymm')
-  os.system('rm FixSymm/yambo.in')
-  return a, b, c, d
-
 # you can select the calculation among : 'collision', 'tdsex', 'pump', 'dissipation'
 
 job = dict()
-job['calculation'] = 'collision'
+job['calculation'] = 'pump'
 job['folder-col']  = 'COLLISION'
-job['folder-run']  = 'PUMP-1e5-80fs'
+job['folder-run']  = ''
 job['DG']          = (False,'DG-60x60')
 job['nodes']       = 1
 job['cores']       = 1
 
-col, tdsex, pump, diss = realtime('.')
-
+if job['calculation']=='collision':
+  run = YamboIn('%s -r -e -v c -V all'%yambo_rt,folder='FixSymm')
+  os.system('rm FixSymm/yambo.in')
+if job['calculation']=='tdsex':
+  run = YamboIn('%s -q p -v c -V all'%yambo_rt,folder='FixSymm')
+if job['calculation']=='pump':
+  run = YamboIn('%s -q p -v c -V all'%yambo_rt,folder='FixSymm')
+if job['calculation']=='dissipation':
+  run = YamboIn('%s -s p -q p -v c -V all'%yambo_rt,folder='FixSymm')
 
 # System Common variables
-col['FFTGvecs']  = [2,'Ha']
-col['EXXRLvcs']  = [2,'Ha']
-col['SCBands']   = [[4,5],'']
+run['FFTGvecs']  = [5,'Ha']
+run['EXXRLvcs']  = [5,'Ha']
+run['SCBands']   = [[4,5],'']
 
-tdsex['FFTGvecs'], tdsex['EXXRLvcs'], tdsex['SCBands'] = col['FFTGvecs'], col['EXXRLvcs'], col['SCBands']
-pump['FFTGvecs'] , pump['EXXRLvcs'] ,  pump['SCBands'] = col['FFTGvecs'], col['EXXRLvcs'], col['SCBands']
-diss['FFTGvecs'] , diss['EXXRLvcs'] ,  diss['SCBands'] = col['FFTGvecs'], col['EXXRLvcs'], col['SCBands']
+# Collision variables
+if job['calculation']=='collision':
+  run['NGsBlkXp']  = [ 100,'mHa']
+  run['BndsRnXs' ] = [[1,30],'']
+  #run['CUTGeo']    = 'box z'               # Only in layers
+  #run['CUTBox']    = [[0,0,38], '']
+  run.write('FixSymm/03_COLLISION')
 
-# Collision
-col['NGsBlkXp']  = [ 100,'mHa']
-col['BndsRnXs' ] = [[1,20],'']
-#col['CUTGeo']    = 'box z'
-#col['CUTBox']    = [[0,0,38], '']
-col.write('FixSymm/03_COLLISION')
+# Common time-dependent variable
+if job['calculation']=='tdsex' or 'pump' or 'dissipation':
+  run['GfnQP_Wv']   = [ [0.10,0.00,0.00],'' ]
+  run['GfnQP_Wc']   = [ [0.10,0.00,0.00],'' ]
+  run['GfnQPdb']    = 'none' 
+  # Time-propagation 
+  run['RTstep']     = [0.50,'fs']
+  run['NETime']     = [ 100,'fs']
+  run['Integrator'] = "RK2 RWA"
+  run['IOtime']     = [ [1.00, 1.00, 1.00], 'fs' ] 
+  # Pump Pulse
+  run['Field1_Int']  = [ 1E6 , 'kWLm2']    # Intensity pulse
+  run['Field1_Dir']  = [[0.0,1.0,0.0],'']  # Polarization pulse
+  run['Field1_pol']  = "linear"            # Polarization type (linear or circular) 
 
 # Time-dependent COHSEX -- DELTA PULSE
-# System
-tdsex['GfnQP_Wv']   = [ [0.04,0.00,0.00],'' ]
-tdsex['GfnQP_Wc']   = [ [0.04,0.00,0.00],'' ]
-tdsex['GfnQPdb']    = 'none' 
-# Time-propagation 
-tdsex['RTstep']     = [0.01,'fs']
-tdsex['NETime']     = [  80,'fs']
-tdsex['Integrator'] = "RK2 RWA"
-tdsex['IOtime']     = [ [0.05, 0.05, 0.01], 'fs' ] 
-# Pump Pulse
-tdsex['Probe_Int']  = [ 1E5 , 'kWLm2']
-tdsex['Probe_kind'] = "DELTA"
-tdsex['Probe_Damp'] = [0,'fs']
-tdsex['Probe_Freq'] = [[0.0,0.0],'eV']
-tdsex['Probe_Dir']  = [[0.0,1.0,0.0],'']
-tdsex.write('FixSymm/04_TDSEX')
+if job['calculation']=='tdsex':
+  run['Field1_kind'] = "DELTA"
+  run.write('FixSymm/04_TDSEX')
 
 # Pumping with finite pulse
-# System
-pump['GfnQP_Wv']   = [ [0.04,0.00,0.00],'' ]
-pump['GfnQP_Wc']   = [ [0.04,0.00,0.00],'' ]
-pump['GfnQPdb']    = 'none' 
-# Time-propagation 
-pump['RTstep']     = [0.01,'fs']
-pump['NETime']     = [   1,'ps']
-pump['Integrator'] = "RK2 RWA"
-pump['IOtime']     = [ [0.05, 0.05, 0.01], 'fs' ] 
-# Pump Pulse
-pump['Probe_Int']  = [ 1E5 , 'kWLm2']
-pump['Probe_kind'] = "QSSIN"
-pump['Probe_Damp'] = [100,'fs']
-pump['Probe_Freq'] = [[2.5,0.0],'eV']
-pump['Probe_Dir']  = [[0.0,1.0,0.0],'']
-pump.write('FixSymm/05_NEGF')
+if job['calculation']=='pump' or 'dissipation':
+  run['Field1_kind'] = "QSSIN"
+  run['Field1_Damp'] = [  2,'fs']
+  run['Field1_Freq'] = [[2.5,0.0],'eV']
+
+if job['calculation']=='pump':
+  run.write('FixSymm/05_NEGF')
 
 # Pumping with finite pulse and electron-phonon dissipation
-# System
-diss['GfnQP_Wv']   = [ [0.04,0.00,0.00],'' ]
-diss['GfnQP_Wc']   = [ [0.04,0.00,0.00],'' ]
-diss['GfnQPdb']    = 'none' 
-# Time-propagation 
-diss['RTstep']     = [0.01,'fs']
-diss['NETime']     = [   1,'ps']
-diss['Integrator'] = "RK2 RWA"
-diss['IOtime']     = [ [0.05, 0.05, 0.01], 'fs' ] 
-diss['LifeInterpKIND']  = 'FLAT'
-diss['LifeInterpSteps'] = [ [10.0,2.5], 'fs' ] 
-# Pump Pulse
-diss['Probe_Int']  = [ 1E5 , 'kWLm2']
-diss['Probe_kind'] = "QSSIN"
-diss['Probe_Damp'] = [100,'fs']
-diss['Probe_Freq'] = [[2.5,0.0],'eV']
-diss['Probe_Dir']  = [[0.0,1.0,0.0],'']
-diss.write('FixSymm/06_DISS')
+if job['calculation']=='dissipation':
+# Interpolation 
+  run['LifeInterpKIND']  = 'FLAT'
+  run['LifeInterpSteps'] = [ [10.0,2.5], 'fs' ] 
+  run.write('FixSymm/06_DISS')
 
-# Run 
+# Run
 
 # Collisions
 
@@ -149,34 +145,46 @@ if job['calculation'] == 'collision':
 # Time-dependent without/with Double Grid 
 
 if job['calculation'] == 'tdsex':
-  print('running TD-COHSEX')
+  job['folder-run'] += 'tds-int-%s' % ( str(int(run['Field1_Int'][0])) )
+  print('running TD-COHSEX in folder: ' + str(job['folder-run']))
+  if not os.path.isdir('FixSymm/'+job['folder-col']):
+    print 'Collisions not found'
+    exit()
   if job['DG'][0]:
     print('with Double Grid from folder %s'%job['DG'][1])
     print('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1]))
-    os.system('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1]))
+    os.system('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s,%s\' -C %s'%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1],job['folder-run']))
   else:
     print ('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
-    os.system ('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
+    os.system ('cd FixSymm; %s -F 04_TDSEX -J \'%s,%s\' -C %s'%(yambo_rt,job['folder-run'],job['folder-col'],job['folder-run']))
 
 # Time-dependent with a pulse and without/with Double Grid 
 
 if job['calculation'] == 'pump':
   print('running pumping with finite pulse')
+  job['folder-run'] += 'negf-int-%s-damp-%sfs-freq-%seV' % ( str(int(run['Field1_Int'][0])), str(run['Field1_Damp'][0]),run['Field1_Freq'][0][0] )
+  print('running NEGF in folder: ' + str(job['folder-run']))
+  if not os.path.isdir('FixSymm/'+job['folder-col']):
+    print 'Collisions not found'
+    exit()
   if job['DG'][0]:
     print('with Double Grid from folder %s'%job['DG'][1])
-    print('%s -F 05_NEGF -J \'%s,%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1]))
+    print('%s -F 05_NEGF -J \'%s,%s,%s\' -C %s'%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1],job['folder-run']))
   else:
     print ('%s -F 05_NEGF -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
-    os.system ('cd FixSymm; %s -F 05_NEGF -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
+    os.system ('cd FixSymm; %s -F 05_NEGF -J \'%s,%s\' -C %s'%(yambo_rt,job['folder-run'],job['folder-col'],job['folder-run']))
 
 # Time-dependent with a pulse and dissipation and without/with Double Grid 
 
 if job['calculation'] == 'dissipation':
-  print('running pumping with finite pulse')
+  print('running pumping with finite pulse and with electron-phonon scattering')
+  print('this run level needs the GKKP folder to run')
+  exit()
+'''
   if job['DG'][0]:
     print('with Double Grid from folder %s'%job['DG'][1])
     print('%s -F 06_DISS -J \'%s,%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col'],job['DG'][1]))
   else:
     print ('%s -F 06_DISS -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
     os.system ('cd FixSymm; %s -F 06_DISS -J \'%s,%s\''%(yambo_rt,job['folder-run'],job['folder-col']))
-
+'''
