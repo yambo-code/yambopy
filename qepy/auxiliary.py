320ea90fc465ce279bf21c2c0a98ddab88b09f4b
alexmoratalla <alejandro.molina@uv.es>
2016-04-22 11:33:05 +0200

    Generation Path for QE
    In auxiliary I have made a function to generate a path. It prints in
    the format of the input file for pw.x but it will also work for
    dynmat.in and I hope for ph.x.

--- qepy/auxiliary.py
+++ qepy/auxiliary.py
@@ -11,3 +11,22 @@ def car_red(car,lat):
     Convert cartesian coordinates to reduced
     """
     return np.array(map( lambda coord: np.linalg.solve(np.array(lat).T,coord), car))
+
+
+def generate_path(klist,kinterval):
+    """
+    Generation of a path in reciprocal space by specifying a list of k-points
+    Output in the format of quantum espresso == [ [kx, ky, kz, 1], ... ]
+    Probably Henrique is going to suffer a hearth attack...
+    """
+    kout  = np.zeros([sum(kinterval)+1,4])
+    kout[:,3] = 1
+    klist = np.array(klist)
+    io = 0
+    for ik,interval in enumerate(kinterval):
+      for ip in range(interval):
+        kout[io,:3] = klist[ik] + float(ip)/interval*(klist[ik+1] - klist[ik])
+        io = io + 1
+    kout[io,:3] = klist[ik] + float(ip+1)/interval*(klist[ik+1] - klist[ik])
+    return kout
+
