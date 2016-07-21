f3164c42469b0f740b6441767803dc07a409ff8a
Henrique Miranda <miranda.henrique@gmail.com>
2016-07-21 09:56:10 +0200

    Added qepy class to run projwfc

--- qepy/projwfcxml.py
+++ qepy/projwfcxml.py
@@ -8,14 +8,14 @@ from   qepy.auxiliary import *
 from   numpy import array, zeros
 import re
 
-HatoeV = 27.2107
+RytoeV = 13.605698066
 
 class ProjwfcXML():
     """ Class to read data from a Quantum espresso XML file
     """
     _proj_file = 'atomic_proj.xml'
 
-    def __init__(self,prefix,output_filename='proj.out',path='.'):
+    def __init__(self,prefix,output_filename='projwfc.log',path='.'):
 
         """ Initialize the structure with the path where the atomic_proj.xml is
         """
@@ -31,15 +31,14 @@ class ProjwfcXML():
         #get number of projections
         self.nproj    = int(self.datafile_xml.find("HEADER/NUMBER_OF_ATOMIC_WFC").text)
 
-        self.eigen = None
-        self.proj  = None
+        self.eigen = self.get_eigen()
+        self.proj  = self.get_proj()
 
-
-        #here we open teh ouput file of projwfc and get the quantum numbers of the orbitals
+        #here we open the ouput file of projwfc and get the quantum numbers of the orbitals
         try:
-            f = open(output_filename,'r')
+            f = open("%s/%s"%(path,output_filename),'r')
         except:
-            print "The output file of projwfc.x was not found"
+            print "The output file of projwfc.x: %s was not found"%output_filename
             exit(1)
 
         states = []
@@ -65,18 +64,16 @@ class ProjwfcXML():
         s += "nbands: %d\n"%self.nbands
         return s
 
-    def get_indexes(self,selected_orbitals):
+    def get_indexes(self):
         """ get indexes of the bands where the projection is maximal
         """
         # Selection of the bands
-        w_proj = zeros([self.nkpoints,self.nbands])
+        proj = zeros([self.nkpoints,self.nproj],dtype=int)
         for ik in range(self.nkpoints):
-          for ib in range(self.nbands):
-            w_proj[ik,ib] = sum(abs(self.proj[ik,selected_orbitals,ib])**2)
+            for ip in range(self.nproj):
+                proj[ik,ip] = np.argmax(np.absolute(self.proj[ik,ip,:])**2)
 
-        #the order is decided according to
-
-        return indexes
+        return proj
 
     def plot_eigen(self, path=[], selected_orbitals=[]):
         """ Plot the band structure. The size of the points is the weigth of
@@ -84,17 +81,14 @@ class ProjwfcXML():
             Under development to include also colormap and a dictionary for the
             selection of the orbitals...
         """
-        import matplotlib.pyplot as plt
-
+        from   matplotlib     import pyplot as plt
+        renormalization = 25.0
         # Font selection and borders
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif',serif="Computer Modern Roman",size=20)
-
-        if self.eigen is None:
-          self.get_eigen()
-
-        if self.proj  is None:
-          self.get_proj()
+        #rcParams['axes.linewidth']    = 3
+        #rcParams['xtick.major.width'] = 2
+        #rcParams['ytick.major.width'] = 2
 
         if path:
             if isinstance(path,Path):
@@ -117,7 +111,7 @@ class ProjwfcXML():
         #plot bands with linewithds
         #for ib in range(self.nbands):
         #    x = range(self.nkpoints)
-        #    y = (self.eigen[:,ib] - self.fermi)*HatoeV
+        #    y = (self.eigen[:,ib] - self.fermi)*RytoeV
         #    points = np.array([x, y]).T.reshape(-1, 1, 2)
         #    segments = np.concatenate([points[:-1], points[1:]], axis=1)
         #    print w_proj[:,ib]
@@ -126,11 +120,11 @@ class ProjwfcXML():
 
         #plot bands
         for ib in range(self.nbands):
-           plt.scatter(range(self.nkpoints),self.eigen[:,ib]*HatoeV - self.fermi*HatoeV,s=w_proj[:,ib]*renormalization,c='r',edgecolors='none')
+           plt.scatter(range(self.nkpoints),self.eigen[:,ib]*RytoeV - self.fermi*RytoeV,s=w_proj[:,ib]*renormalization,c='r',edgecolors='none')
 
         plt.xlim(0, self.nkpoints-1)
         lim = 0.1
-        plt.ylim(min(self.eigen.flatten()-lim)*HatoeV, max(self.eigen.flatten()+lim)*HatoeV)
+        plt.ylim(min(self.eigen.flatten()-lim)*RytoeV, max(self.eigen.flatten()+lim)*RytoeV)
         plt.show()
 
     def get_eigen(self):
@@ -143,6 +137,12 @@ class ProjwfcXML():
         self.eigen = np.array(eigen)
         return eigen
 
+    def write_proj(self,filename='proj'):
+        """
+        Write the projection array in a numpy file
+        """
+        np.savez(filename,proj=self.proj)
+        
     def get_proj(self):
         """ Return projections
         """
@@ -159,5 +159,5 @@ class ProjwfcXML():
         s  = "nbands:   %d\n"%self.nbands
         s += "nkpoints: %d\n"%self.nkpoints
         for n,state in enumerate(self.states):
-            s += "n -> iatom:%3d atype:%2s wfc:%d\n"%(state['iatom'],state['atype'],state['wfc'])
+            s += "n -> iatom:%3d atype:%2s wfc:%d j:%s l:%s m:%s m_j:%s\n"%(state['iatom'],state['atype'],state['wfc'],str(state['j']),str(state['l']),str(state['m']),str(state['m_j']))
         return s
