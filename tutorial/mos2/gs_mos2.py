b7620cf670161e6cd99f68f3c3399471173eb77d
Henrique Miranda <miranda.henrique@gmail.com>
2016-08-01 16:07:01 +0200

    Added functions to plot bandstructure in MoS2

--- tutorial/mos2/gs_mos2.py
+++ tutorial/mos2/gs_mos2.py
@@ -5,6 +5,17 @@
 from __future__ import print_function, division
 from qepy import *
 import argparse
+import sys
+
+scf_kpoints  = [12,12,1]
+nscf_kpoints  = [12,12,1]
+nscf2_kpoints = [24,24,1]
+prefix = 'mos2'
+
+p = Path([ [[0.0, 0.0, 0.0],'G'],
+           [[0.5, 0.0, 0.0],'M'],
+           [[1./3,1./3,0.0],'K'],
+           [[0.0, 0.0, 0.0],'G']], [20,20,20])
 
 #
 # Create the input files
@@ -30,7 +41,7 @@ def get_inputfile():
     qe.system['nat'] = 3
     qe.system['ntyp'] = 2
     qe.system['ibrav'] = 4
-    qe.kpoints = [9, 9, 1]
+    qe.kpoints = scf_kpoints
     qe.electrons['conv_thr'] = 1e-8
     return qe
 
@@ -66,6 +77,20 @@ def nscf(kpoints,folder):
     qe.kpoints = kpoints
     qe.write('%s/mos2.nscf'%folder)
 
+#bands
+def bands():
+    if not os.path.isdir('bands'):
+        os.mkdir('bands')
+    qe = get_inputfile()
+    qe.control['calculation'] = "'bands'"
+    qe.electrons['diago_full_acc'] = ".true."
+    qe.electrons['conv_thr'] = 1e-10
+    qe.system['nbnd'] = 8
+    qe.system['force_symmorphic'] = ".true."
+    qe.ktype = 'crystal'
+    qe.set_path(p)
+    qe.write('bands/%s.bands'%prefix)
+
 def update_positions(pathin,pathout):
     """ update the positions of the atoms in the scf file using the output of the relaxation loop
     """
@@ -83,18 +108,28 @@ if __name__ == "__main__":
 
     #parse options
     parser = argparse.ArgumentParser(description='Test the yambopy script.')
-    parser.add_argument('-r' ,'--relax',       action="store_false", help='Don\'t run structural relaxation')
-    parser.add_argument('-s' ,'--scf',         action="store_false", help='Don\'t run self-consistent calculation')
-    parser.add_argument('-n' ,'--nscf',        action="store_false", help='Don\'t run non-self consistent calculation')
-    parser.add_argument('-n2','--nscf_double', action="store_false", help='Don\'t run non-self consistent calculation for the double grid')
-    parser.add_argument('-t' ,'--nthreads',    action="store_true", help='Number of threads', default=2 )
+    parser.add_argument('-r' ,'--relax',       action="store_true", help='Structural relaxation')
+    parser.add_argument('-s' ,'--scf',         action="store_true", help='Self-consistent calculation')
+    parser.add_argument('-n' ,'--nscf',        action="store_true", help='Non-self consistent calculation')
+    parser.add_argument('-n2','--nscf_double', action="store_true", help='Non-self consistent calculation for the double grid')
+    parser.add_argument('-b' ,'--bands',       action="store_true", help='Calculate band-structure')
+    parser.add_argument('-p' ,'--phonon',      action="store_true", help='Phonon calculation')
+    parser.add_argument('-t' ,'--nthreads',    help='Number of threads', default=2 )
     args = parser.parse_args()
+
+    nthreads = int(args.nthreads)
+    print( "Using %d threads"%nthreads )
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
 
     # create input files and folders
     relax()
     scf()
-    nscf([9,9,1], 'nscf')
-    nscf([18,18,1], 'nscf_double')
+    nscf(nscf_kpoints,  'nscf')
+    nscf(nscf2_kpoints, 'nscf_double')
+    bands()
 
     if args.relax:
         print("running relax:")
@@ -118,3 +153,14 @@ if __name__ == "__main__":
         os.system("cp -r scf/mos2.save nscf_double/") #nscf
         os.system("cd nscf_double; mpirun -np %d pw.x -inp mos2.nscf > nscf_double.log"%args.nthreads) #nscf
         print("done!")
+
+
+    if args.bands:
+        print("running bands:")
+        os.system("cp -r scf/%s.save bands/"%prefix)
+        os.system("cd bands; mpirun -np %d pw.x -inp %s.bands | tee bands.log"%(nthreads,prefix))
+        print("done!")
+
+        print("running plotting:")
+        xml = PwXML(prefix=prefix,path='bands')
+        xml.plot_eigen(p)
