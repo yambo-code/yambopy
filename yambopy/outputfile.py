Fixed regexp to capture the tags from gw calculation

yambopy/outputfile.py
@@ -84,7 +84,7 @@ class YamboOut():
         for filename,f in zip(self.output,files):
             #get the string with the file data
             string = f.read()
-            tags = [tag.strip() for tag in re.findall('([ `0-9a-zA-Z\-\/]+)\[[0-9]\]',string)]
+            tags = [tag.strip() for tag in re.findall('#\n#\s+((?:(?:[`0-9a-zA-Z\-\/\|\[\]]+)\s+)+)#\n\s',string)[0].split()]
             f.seek(0)
             self.data[filename] = np.loadtxt(f)
             self.tags[filename] = tags