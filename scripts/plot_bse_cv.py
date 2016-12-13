f63e843f034f45eaec1d6c9f1ef3fc20e9920c85
Alexandre Morlet <morlet@crans.org>
2016-12-13 14:35:33 +0100

    tabs -> spaces

--- scripts/plot_bse_cv.py
+++ scripts/plot_bse_cv.py
@@ -1,5 +1,5 @@
 # Version : 12th dec
-""" python plot_bse_cv.py <PathToFolder> <Variable> 
+""" python plot_bse_cv.py <PathToFolder> <Variable>
 This script allows to track the energy of the n-first bright exciton (settings in script).
 It also creates a .png file with all the absorption spectra on top of each other.
 
@@ -70,8 +70,8 @@ excitons = []
 
 # Loop over all calculations
 for key in keys:
-	jobname=key.replace('.json','')
-	print jobname
+    jobname=key.replace('.json','')
+    print jobname
 
         # input value
         # BndsRn__ is a special case
@@ -81,39 +81,39 @@ for key in keys:
         else:
             inp = invars[key]['variables'][var][0]
 
-	print 'Preparing JSON file. Calling ypp if necessary.'
-	### Creating the 'absorptionspectra.json' file
-	# It will contain the exciton energies
-	y = YamboOut(folder=path,save_folder=path)
-	# Args : name of job, SAVE folder path, folder where job was run path
-	a = YamboBSEAbsorptionSpectra(jobname,path=path)
-	# Get excitons values
-	a.get_excitons(min_intensity=exc_int,max_energy=exc_max_E,Degen_Step=exc_degen)
-	# Get excitonic WFs (contains eigenvalues)
-	a.get_wavefunctions(Degen_Step=exc_degen,repx=range(-1,2),repy=range(-1,2),repz=range(1))
-	# Write .json file
-	a.write_json(filename=path+'_'+jobname)
+    print 'Preparing JSON file. Calling ypp if necessary.'
+    ### Creating the 'absorptionspectra.json' file
+    # It will contain the exciton energies
+    y = YamboOut(folder=path,save_folder=path)
+    # Args : name of job, SAVE folder path, folder where job was run path
+    a = YamboBSEAbsorptionSpectra(jobname,path=path)
+    # Get excitons values
+    a.get_excitons(min_intensity=exc_int,max_energy=exc_max_E,Degen_Step=exc_degen)
+    # Get excitonic WFs (contains eigenvalues)
+    a.get_wavefunctions(Degen_Step=exc_degen,repx=range(-1,2),repy=range(-1,2),repz=range(1))
+    # Write .json file
+    a.write_json(filename=path+'_'+jobname)
 
-	### Loading data from .json file
-	f = open(path+'_'+key)
-	data = json.load(f)
-	f.close()
-	print 'JSON file prepared and loaded.'
+    ### Loading data from .json file
+    f = open(path+'_'+key)
+    data = json.load(f)
+    f.close()
+    print 'JSON file prepared and loaded.'
 
-	### Plotting the absorption spectra
-	# BSE spectra
-	plt.plot(data['E/ev[1]'], data['EPS-Im[2]'],label=jobname,lw=2)
-#	# Axes : lines for exciton energies, labels
-#	for n,exciton in enumerate(data['excitons']):
-#	    plt.axvline(exciton['energy'])
+    ### Plotting the absorption spectra
+    # BSE spectra
+    plt.plot(data['E/ev[1]'], data['EPS-Im[2]'],label=jobname,lw=2)
+#   # Axes : lines for exciton energies, labels
+#   for n,exciton in enumerate(data['excitons']):
+#       plt.axvline(exciton['energy'])
 
-	### Creating array with exciton values (according to settings)
-	l = [inp]
-	for n,exciton in enumerate(data['excitons']):
-		if n <= exc_n-1:
-			l.append(exciton['energy'])
+    ### Creating array with exciton values (according to settings)
+    l = [inp]
+    for n,exciton in enumerate(data['excitons']):
+        if n <= exc_n-1:
+            l.append(exciton['energy'])
 
-	excitons.append(l)
+    excitons.append(l)
 
 header = 'Variable: '+var+', unit: '+unit
 np.savetxt(outname,excitons,header=header,fmt='%1f')
