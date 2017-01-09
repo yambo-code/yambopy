d53326c2c59afbe83f3807510499e41ff2c3ffda
tatouille <morlet@crans.org>
2017-01-09 11:33:51 +0100

    Now with automatic plotting

--- scripts/rt_delta_abs_json.py
+++ scripts/rt_delta_abs_json.py
@@ -1,13 +1,16 @@
+import matplotlib
+#matplotlib.use('Agg') # prevents crashes if no X server present
 from yambopy import *
 import sys
 import os
 import argparse
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
-Input: folder of real-time simulations & jobname
-Output: plot of difference between t0 and tX for each X
+After real-time calculations are completed, plots the Transient Absorption (TA) at different times.
+Setting inside the script : prefix
 """
 
 parser = argparse.ArgumentParser(description='Map of a double-grid')
@@ -18,18 +21,21 @@ args = parser.parse_args()
 print "Folder: ",args.folder
 print "Job: ", args.job
 
+folder = args.folder
+job = args.job
+
 prefix = 'B-' # (B)SE, (IP)
 print 'Prefix: "B-"'
 
-#pack_files_in_folder(args.folder)
+#pack_files_in_folder(folder)
 
-data = YamboAnalyser(args.folder)
-output = data.get_data(args.job)
+data = YamboAnalyser(folder)
+output = data.get_data(job)
 
 # keys to read the outputs in order
 keys=[]
 for key in output:
-    if key.startswith(prefix+args.job):
+    if key.startswith(prefix+job):
         keys.append(key)
 
 keys=sorted(keys)
@@ -74,14 +80,14 @@ for l in range(0,nlines):
 
 # Writing
 
-file1=open(args.job+'.dat','w')
+file1=open(job+'.dat','w')
 string = 'eV'
 for t in times:
     string = string + '\t' + t
 np.savetxt(file1,array,delimiter='\t',header=string)
 file1.close()
 
-file2=open(args.job+'_diff.dat','w')
+file2=open(job+'_diff.dat','w')
 string = 'eV'
 for i,t in enumerate(times):
     if i==0:
@@ -89,5 +95,17 @@ for i,t in enumerate(times):
     string = string + '\t' + t + '-t0'
 np.savetxt(file2,diff,delimiter='\t',header=string)
 file2.close()
+
+print 'Writing done.'
+
+# Plotting
+for i in range(1,len(diff[0])):
+    plt.plot(diff[:,0],diff[:,i],'-',label='%s-%s'%(times[i],times[0]))
+plt.legend(loc=2)
+plt.xlabel("eV")
+plt.ylabel("TA (arb. units)")
+plt.title("TA, job %s in folder %s"%(job,folder))
+plt.show()
+
 
 print 'Done.'
