7dd2195ee4b67f962a6d5d3505a1efa16bb82b87
alexmoratalla <alejandro.molina@uv.es>
2017-01-10 10:29:29 +0100

    bug in qp_correction plot

--- yambopy/analyse.py
+++ yambopy/analyse.py
@@ -282,8 +282,8 @@ class YamboAnalyser():
        box = ax.get_position()
        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size':8})
-       plt.show()
        plt.plot()
+       plt.show()
 
     def plot_gw(self,tags=('qp',),cols=(lambda x: x[2]+x[3],),rows=None):
         """ Use this function to plot the quasiparticle energies from a GW calculation
