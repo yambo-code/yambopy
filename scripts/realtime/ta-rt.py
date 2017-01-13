4e0c63a9308286038762b90d68622aacf6f290dd
tatouille <morlet@crans.org>
2017-01-13 17:24:57 +0100

    rt_delta[...] updated and renamed in more clear ta-rt

--- scripts/realtime/ta-rt.py
+++ scripts/realtime/ta-rt.py
@@ -14,8 +14,8 @@ Setting inside the script : prefix
 """
 
 parser = argparse.ArgumentParser(description='Map of a double-grid')
-parser.add_argument('-f' ,'--folder'    , help='Folder with real-time simulations')
-parser.add_argument('-j' ,'--job'       , help='Name of job (ex: DELTA-1E+03)')
+parser.add_argument('-f' ,'--folder'    , help='Folder with data for TA')
+parser.add_argument('-j' ,'--job'       , help='Name of job (ex: B-QSSIN-... without "-tX")')
 args = parser.parse_args()
 
 print "Folder: ",args.folder
@@ -24,31 +24,25 @@ print "Job: ", args.job
 folder = args.folder
 job = args.job
 
-prefix = 'B-' # (B)SE, (IP)
-print 'Prefix: "B-"'
-
-#pack_files_in_folder(folder)
+#print 'packing'
+#pack_files_in_folder(folder,mask=job)
+#print 'done'
 
 data = YamboAnalyser(folder)
-output = data.get_data(job)
+output = data.get_data((job,'eps'))
 
 # keys to read the outputs in order
-keys=[]
-for key in output:
-    if key.startswith(prefix+job):
-        keys.append(key)
-
+keys=output.keys()
 keys=sorted(keys)
-print keys
+print 'Keys : ',keys
 
 # times to print in file
 times=[]
 for i in range(0,len(keys)):
     t=re.search("[t]\d{1,}", keys[i]).group()
-    print t
     times.append(t)
 
-print times
+print 'Times : ',times
 
 # output[key][n][0] is x
 # output[key][n][1] is y
@@ -99,12 +93,16 @@ file2.close()
 print 'Writing done.'
 
 # Plotting
+fig,ax1=plt.subplots()
 for i in range(1,len(diff[0])):
     plt.plot(diff[:,0],diff[:,i],'-',label='%s-%s'%(times[i],times[0]))
 plt.legend(loc=2)
 plt.xlabel("eV")
 plt.ylabel("TA (arb. units)")
+plt.axvline(1.94)
 plt.title("TA, job %s in folder %s"%(job,folder))
+ax2 = ax1.twinx()
+#plt.plot(array[:,0],array[:,1],'k-',label='BSE@t0')
 plt.show()
 
 
