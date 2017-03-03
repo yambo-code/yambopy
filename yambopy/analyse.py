7c82083c21f7ed8e99d8821a83a0c5422e443791
tatouille <morlet@crans.org>
2017-03-03 15:11:20 +0100

    Add ylabel for GW plot

--- yambopy/analyse.py
+++ yambopy/analyse.py
@@ -228,6 +228,7 @@ class YamboAnalyser():
 
             box = ax.get_position()
             plt.title('GW quasiparticles on a path')
+            plt.ylabel('E (eV)')
             ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
             plt.xlim(0,max(bands_distances))
             ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size':8})
