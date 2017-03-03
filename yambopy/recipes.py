0a3262d91743db439e9463d23aab198abc3fb27b
tatouille <morlet@crans.org>
2017-03-03 18:13:59 +0100

    Added analyse_gw to yambopy exc, --pack & --text flags not working

--- yambopy/recipes.py
+++ yambopy/recipes.py
@@ -49,6 +49,101 @@ def breaking_symmetries(efield1,efield2=[0,0,0],folder='.',RmTimeRev=True):
     os.system('mv %s/FixSymm/SAVE %s/'%(folder,folder))
     os.system('rm -r %s/FixSymm'%folder)
 
+
+#
+# by Alexandre Morlet
+#
+
+def analyse_gw(folder,var,bandc,kpointc,bandv,kpointv,pack,text):
+    """
+    Study the convergence of GW calculations by looking at the change in band-gap value.
+
+    The script reads from <folder> all results from <variable> calculations and display them.
+
+    Use the band and k-point options (or change default values) according to the size of your k-grid and
+    the location of the band extrema.
+    """
+
+    print 'Valence band: ',bandv,'conduction band: ',bandc
+    print 'K-point VB: ',kpointv, ' k-point CB: ',kpointc
+
+
+    # Packing results (o-* files) from the calculations into yambopy-friendly .json files
+    if pack:
+        print 'Packing ...'
+        pack_files_in_folder(folder,mask=var)
+        pack_files_in_folder(folder,mask='reference')
+    else:
+        print 'Packing skipped.'
+
+    # importing data from .json files in <folder>
+    print 'Importing...'
+    data = YamboAnalyser(folder)
+
+    # extract data according to relevant variable
+    outvars = data.get_data(var)
+    invars = data.get_inputfiles_tag(var)
+    tags = data.get_tags(var)
+
+    # Get only files related to the convergence study of the variable,
+    # ordered to have a smooth plot
+    keys=[]
+    sorted_invars = sorted(invars.items(), key=operator.itemgetter(1))
+
+    for i in range(0,len(sorted_invars)):
+        key=sorted_invars[i][0]
+        if key.startswith(var) or key=='reference.json':
+            keys.append(key)
+    print 'Files detected: ',keys
+
+    print 'Preparing output...'
+    ### Output
+
+    # Unit of the variable :
+    unit = invars[keys[0]]['variables'][var][1]
+
+    # The following variables are used to make the script compatible with both short and extended output
+    kpindex = tags[keys[0]].tolist().index('K-point')
+    bdindex = tags[keys[0]].tolist().index('Band')
+    e0index = tags[keys[0]].tolist().index('Eo')
+    gwindex = tags[keys[0]].tolist().index('E-Eo')
+
+
+    array = np.zeros((len(keys),2))
+
+    for i,key in enumerate(keys):
+        # input value
+        # GbndRnge and BndsRnX_ are special cases
+        if var.startswith('GbndRng') or var.startswith('BndsRnX'):
+            # format : [1, nband, ...]
+            array[i][0] = invars[key]['variables'][var][0][1]
+        else:
+            array[i][0] = invars[key]['variables'][var][0]
+
+        # Output value (gap energy)
+        # First the relevant lines are identified
+        valence=[]
+        conduction=[]
+        for j in range(len(outvars[key]+1)):
+            if outvars[key][j][kpindex]==kpointc and outvars[key][j][bdindex]==bandc:
+                    conduction=outvars[key][j]
+            elif outvars[key][j][kpindex]==kpointv and outvars[key][j][bdindex]==bandv:
+                    valence = outvars[key][j]
+        # Then the gap can be calculated
+        array[i][1] = conduction[e0index]+conduction[gwindex]-(valence[e0index]+valence[gwindex])
+
+    if text:
+        os.system('mkdir -p analyse_%s'%folder)
+        outname = './analyse_%s/%s_%s.dat'%(folder,folder,var)
+        header = var+' ('+str(unit)+'), gap'
+        np.savetxt(outname,array,delimiter='\t',header=header)
+        print outname
+
+    plt.plot(array[:,0],array[:,1],'o-')
+    plt.xlabel(var+' ('+unit+')')
+    plt.ylabel('E_gw = E_lda + \Delta E')
+    plt.show()
+
 #
 # by Alexandre Morlet
 #
@@ -72,7 +167,7 @@ def analyse_bse( folder, var, numbexc=2, intexc=0.05, degenexc=0.01, maxexc=8.0,
         text     -> Skips writing the .dat file (default: True)
         draw     -> Skips drawing (plotting) the abs spectra (default: True)
     """
-            
+
     # Packing results (o-* files) from the calculations into yambopy-friendly .json files
     if pack: # True by default, False if -np used
         print 'Packing ...'
@@ -163,8 +258,6 @@ def analyse_bse( folder, var, numbexc=2, intexc=0.05, degenexc=0.01, maxexc=8.0,
         np.savetxt(outname+'.dat',excitons,header=header)
         #np.savetxt(outname,excitons,header=header,fmt='%1f')
         print outname+'.dat'
-    else:
-        print '-nt flag : no text produced.'
 
     if draw:
         plt.xlabel('$\omega$ (eV)')
@@ -197,7 +290,7 @@ def merge_qp(output,files,verbose=False):
                 print ''.join(d['DESC_strings_%05d'%i][0])
         else:
             print "description:", ''.join(d['DESC_strings_%05d'%(nstrings)][0])
-        print 
+        print
         QP_table.append( d['QP_table'][:].T )
         QP_kpts.append( d['QP_kpts'][:].T )
         QP_E_E0_Z.append( d['QP_E_Eo_Z'][:] )
@@ -221,11 +314,11 @@ def merge_qp(output,files,verbose=False):
 
     #create reference file from one of the files
     fin  = datasets[0]
-    fout = Dataset(output,'w') 
+    fout = Dataset(output,'w')
 
     variables_update = ['QP_table', 'QP_kpts', 'QP_E_Eo_Z']
     variables_save   = [QP_table_save.T, QP_kpts_save.T, QP_E_E0_Z_save]
-    variables_dict   = dict(zip(variables_update,variables_save)) 
+    variables_dict   = dict(zip(variables_update,variables_save))
     PARS_save = fin['PARS'][:]
     PARS_save[1:3] = nkpoints,len(QP_table_save)
 
@@ -278,5 +371,5 @@ def merge_qp(output,files,verbose=False):
                 outVar[:,:len(description_save)] = description_save.T
             else:
                 outVar[:] = varin[:]
-            
+
     fout.close()
