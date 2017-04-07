b60a68b10d5601923a1f2298c4b227ff633a885d
Henrique Miranda <miranda.henrique@gmail.com>
2017-04-07 14:59:28 +0200

    Added option to write a txt file with the dielectric screening as funciton of q

--- yambopy/dbs/latticedb.py
+++ yambopy/dbs/latticedb.py
@@ -10,34 +10,6 @@ import operator
 
 atol = 1e-6
 
-def vec_in_list(veca,vec_list):
-    """ check if a vector exists in a list of vectors
-    """
-    return np.array([ np.allclose(veca,vecb,rtol=atol,atol=atol) for vecb in vec_list ]).any()
-
-def red_car(red,lat):
-    """
-    Convert reduced coordinates to cartesian
-    """
-    return np.array(map( lambda coord: coord[0]*lat[0]+coord[1]*lat[1]+coord[2]*lat[2], red))
-
-def car_red(car,lat):
-    """
-    Convert cartesian coordinates to reduced
-    """
-    return np.array(map( lambda coord: np.linalg.solve(np.array(lat).T,coord), car))
-
-def rec_lat(lat):
-    """
-    Calculate the reciprocal lattice vectors
-    """
-    a1,a2,a3 = np.array(lat)
-    v = np.dot(a1,np.cross(a2,a3))
-    b1 = np.cross(a2,a3)/v
-    b2 = np.cross(a3,a1)/v
-    b3 = np.cross(a1,a2)/v
-    return np.array([b1,b2,b3])
-
 class YamboLatticeDB():
     def __init__(self, save='SAVE',filename='ns.db1',expand=True):
         self.filename = '%s/%s'%(save,filename)
