c247811d93cb584f81fd05d94cd20b7b0c22c770
tatouille <morlet@crans.org>
2017-04-19 14:46:37 +0200

    test install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -1,17 +1,10 @@
-/usr/bin/env sh
+#/usr/bin/env sh
 
 set -e # terminate if error is encountered
 set -x # print to stdout
 
-# Parse args
-while echo $1 | grep -q ^-; do
-    eval $( echo $1 | sed 's/^-//' )=$2
-    shift
-    shift
-done
-
-echo "YAMBO_VERSION = $y"
-echo "PW_VERSION    = $pw"
+echo $YAMBO_VERSION
+echo $PW_VERSION
 
 wget https://www.dropbox.com/s/7g2wxesc0sxu19h/yambo?dl=0 -O yambo # yambo 4.1
 chmod +x yambo
