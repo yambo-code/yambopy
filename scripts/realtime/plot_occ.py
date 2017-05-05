a63573033276b089d0fbad67e9ef78eaa8c785e9
tatouille <morlet@crans.org>
2017-05-05 18:39:35 +0200

    plot_occ.py v2

--- scripts/realtime/plot_occ.py
+++ scripts/realtime/plot_occ.py
@@ -1,5 +1,6 @@
 # This script reads the carrier database
 # and display it along a path in histogram form
+# along with a representation of the carriers in energy space
 
 from yambopy import *
 import matplotlib.gridspec as gridspec
@@ -10,9 +11,8 @@ from scipy.optimize import curve_fit
 # SETTINGS #
 ############
 
-#save = 'rt-24x24/SAVE' # Save with the appropriate band structure
 folder = 'rt-24x24'
-calc   = 'QSSIN-D-100.0fs-2.07eV-300K-DG' # Where RT carrier output is
+calc   = 'QSSIN-100.0fs-2.07eV-300K' # Where RT carrier output is
 path = [[0.0,0.0,0.0],[0.5,0.0,0.0],[0.33333,0.33333,0.0],[0.0,0.0,0.0]]
 nbv = 2 ; nbc = 2 # nb of valence and conduction bands
 occ_scaling = 1 # max occupation will be 1eV high
@@ -126,73 +126,96 @@ if nbv > 1:
 # Gridspec allows to place subplots on a grid
 # spacing for exemple can be customised
 gs = gridspec.GridSpec(9, 8)
-#gs.update(hspace=1.1) # bigger horizontal spacing
 
+# y range for band structure & energy plots
+ymin_v= np.amin(eigenvalues[:,:nbv])-0.1
+ymin_c= np.amin(eigenvalues[:,nbv:])-0.1
+
+ymax_v= max(np.amax(eigenvalues[:,:nbv])+np.amax(occ_c[:,:,nbv:])+0.1, np.amax(eigenvalues[:,:nbv])+0.1)
+ymax_c= max(np.amin(eigenvalues[:,nbv:])+np.amax(occ_c[:,:,nbv:])+0.1, np.amax(eigenvalues[:,nbv:])+0.1)
+###
 
 for t in range(len(times)):
     i=t
     print times[i]
 
-    plt.figure().suptitle('Occupation of the bands and fit to the Fermi-Dirac distribution',fontsize=14,ha='center')
+    fig = plt.figure()
+
+    fig.suptitle('Occupation of the bands and fit to the Fermi-Dirac distribution',fontsize=14,ha='center')
 
 ####### bandstructure w/ occupation plot
 
-    ax1 = plt.subplot(gs[0:-1,0:-2])
+    ax1c = plt.subplot(gs[0:4,0:-2])
+    ax1v = plt.subplot(gs[4:8,0:-2])
     # remove x ticks
-    ax1.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
+    ax1c.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
+    ax1v.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
 
-    ax1.set_xlim((0,xocc[-1]))
+    # set x range
+    ax1c.set_xlim((0,xocc[-1]))
+    ax1v.set_xlim((0,xocc[-1]))
+    # y range is defined with ax3 and ax4 (they share y axis with ax1)
+
 
     # Plot band structure
-    ax1.plot(eigenvalues,'k-',lw=2,zorder=0)
+    ax1v.plot(eigenvalues[:,:nbv],'k-',lw=2,zorder=0)
+    ax1c.plot(eigenvalues[:,nbv:],'k-',lw=2,zorder=0)
 
-    ## Colored lines when degen is beyond degen_thres
+    ## Colored spots when degen is beyond degen_thres
     # For that, we compare eigenvalues of (k,n) with (k,n+1)
-    # eigenvalues[kindex,n], nbc, nbv
-# TODO : loop using parameters
+# note : if more than 2 VB/CB, this scatter scheme might not be optimal (e.g. with 1 + 2 degen bands)
+
     # VB
-    diff_eigen = abs(eigenvalues[:,0]-eigenvalues[:,1])
-    ax1.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,0],s=30, c=occ_v[t,diff_eigen>degen_thres,0],cmap='plasma',alpha=1,edgecolors='none',norm=norm)
-    ax1.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,1],s=30, c=occ_v[t,diff_eigen>degen_thres,1],cmap='plasma',alpha=1,edgecolors='none',norm=norm)
+    for n in range(nbv-1): # we compare n and n+1 <= nbv
+        # bool array with condition on degeneracy
+        diff_eigen = abs(eigenvalues[:,n]-eigenvalues[:,n+1])
+        # plot for points that respect the condition
+        ax1v.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,n]  ,s=30, c=occ_v[t,diff_eigen>degen_thres,n]  ,cmap='plasma',alpha=1,edgecolors='none',norm=norm)
+        ax1v.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,n+1],s=30, c=occ_v[t,diff_eigen>degen_thres,n+1],cmap='plasma',alpha=1,edgecolors='none',norm=norm)
+
     # CB
-    diff_eigen = abs(eigenvalues[:,2]-eigenvalues[:,3])
-    ax1.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,2],s=30, c=occ_v[t,diff_eigen>degen_thres,0],cmap='bwr',alpha=1,edgecolors='none')
-    ax1.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,3],s=30, c=occ_v[t,diff_eigen>degen_thres,0],cmap='bwr',alpha=1,edgecolors='none')
+    for n in range(nbc-1):
+        diff_eigen = abs(eigenvalues[:,nbv+n]-eigenvalues[:,nbv+n+1])
+        ax1c.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,nbv+n]  ,s=30, c=occ_c[t,diff_eigen>degen_thres,n]  ,cmap='plasma',alpha=1,edgecolors='none',norm=norm)
+        ax1c.scatter(xocc[diff_eigen>degen_thres],eigenvalues[diff_eigen>degen_thres,nbv+n+1],s=30, c=occ_c[t,diff_eigen>degen_thres,n+1],cmap='plasma',alpha=1,edgecolors='none',norm=norm)
 
     ## occupation in the form of histograms
     # small y-shift for better reading
-    ax1.bar(xocc,occ_v[t,:,nbv],width=0.4,bottom=eigenvalues[:,nbv-1]+0.1,color='blue',edgecolor='none')
-    ax1.bar(xocc,occ_c[t,:,nbc],width=0.4,bottom=eigenvalues[:,nbands-1]+0.1,color='red',edgecolor='none')
+    ax1v.bar(xocc,occ_v[t,:,nbv],width=0.4,bottom=eigenvalues[:,nbv-1]+0.1,color='blue',edgecolor='none')
+    ax1c.bar(xocc,occ_c[t,:,nbc],width=0.4,bottom=eigenvalues[:,nbands-1]+0.1,color='red',edgecolor='none')
 
     # text and labels
-    ax1.set_ylabel('E (eV)')
-    ax1.text(0.10,0.05, '%d fs'%times[t],size=16,transform=ax1.transAxes)
+    fig.text(0.05,0.6,'Energy (eV)',size=16,rotation='vertical')
+    fig.text(0.50,0.91, '%d fs'%times[t],size=16)
 
 ######## field plot
 
     ax2 = plt.subplot(gs[-1,:])
+
+    # remove ticks and labels
     ax2.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
     ax2.tick_params(axis='y',which='both',left='off',right='off',labelleft='off')
+
+    # text
     ax2.set_ylabel('Field')
+
+    # frame size
     ax2.set_xlim((0,times[-1]))
     ax2.set_ylim((-1.3,1.3))
+
     ax2.plot(field[:int(times[t])])
 
-## Rotated version
-    ax3 = plt.subplot(gs[0:4,-2:])
-    ax4 = plt.subplot(gs[4:8,-2:])
-    ax3.text(0.98,0.5,'Energy (eV)',rotation='vertical')
-#
-## Make the spacing between the two ax3es a bit smaller
-#    plt.subplots_adjust(wspace=0.1)
-#
-# plot the same data on both ax3es
+## Plot of the occupation as a function of energy (rotated to match the band structure)
+    ax3 = plt.subplot(gs[0:4,-2:],sharey=ax1c)
+    ax4 = plt.subplot(gs[4:8,-2:],sharey=ax1v)
+
+# plot the data
     try: # does not break if fit is not found
         fit,cov = curve_fit(fermi_dirac,occ_e[i,:,0],occ_e[i,:,1])
     except RuntimeError:
         fit=np.array([0,0])
 
-    ax3.scatter(occ_e[i,:,1],occ_e[i,:,0],color='black')
+    ax3.scatter(occ_e[i,:,1],occ_e[i,:,0],s=10,color='black')
     ax3.plot(fermi_dirac(xeng,fit[0],fit[1]),xeng,'r-')
     ax3.text(0.5,0.9,'Electrons\nT = %d K'%(fit[1]/KtoeV),transform=ax3.transAxes,ha='center',va='center')
 
@@ -205,19 +228,19 @@ for t in range(len(times)):
     ax4.plot(fermi_dirac(xeng,fit[0],fit[1]),-xeng,'b-')
     ax4.text(0.5,0.1,'Holes\nT = %d K'%(fit[1]/KtoeV),transform=ax4.transAxes,ha='center',va='center')
 
-# zoom-in / limit the view to different portions of the data
-    ax3.set_ylim(min(occ_e[i,:,0])-0.1,max(occ_e[i,:,0])+0.1)
-    ax4.set_ylim(min(-occ_h[i,:,0])-0.1,max(-occ_h[i,:,0])+0.1)
-
+# set x and y range
     ax4.set_xlim(-0.1*max_occ,1.1*max_occ)
     ax3.set_xlim(-0.1*max_occ,1.1*max_occ)
 
+    ax3.set_ylim(( ymin_c,ymax_c ))
+    ax4.set_ylim(( ymin_v,ymax_v ))
+
 # hide some ticks/labels
     ax3.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
-    ax3.yaxis.tick_right()
+    ax3.tick_params(axis='y',labelleft='off',labelright='off')
 
     ax4.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
-    ax4.yaxis.tick_right()
+    ax4.tick_params(axis='y',labelleft='off',labelright='off')
 
     plt.show()
 
