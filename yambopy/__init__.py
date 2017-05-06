1e0a620e23a0992b1b02766dfc416edad9ad8b9c
Henrique Miranda <miranda.henrique@gmail.com>
2017-05-06 18:21:16 +0200

    Added YamboWFDB class to read/write the wavefunctions

--- yambopy/__init__.py
+++ yambopy/__init__.py
@@ -48,6 +48,7 @@ from yambopy.dbs.greendb import *
 from yambopy.dbs.latticedb import *
 from yambopy.dbs.electronsdb import *
 from yambopy.dbs.excitondb import *
+from yambopy.dbs.wfdb import *
 
 #input/output files
 from yambopy.io.inputfile import *
