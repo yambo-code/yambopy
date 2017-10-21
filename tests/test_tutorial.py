68596cde49dfe5823ab362557e241dd7b537afb0
Henrique Miranda <miranda.henrique@gmail.com>
2017-10-21 09:34:02 +0200

    Changed clean in test_tutorial

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -96,7 +96,8 @@ def is_exe(fpath):
 
 def clean():
         print("cleaning...")
-        os.system('rm -rf relax gw bse_conv bse_cutoff bse_par bse gw_conv bands scf nscf database proj.in')
+        os.system('rm -rf relax gw bse_conv bse_cutoff bse_cutoff_cut '
+                  'bse_par bse gw_conv bands scf nscf database proj.in')
         print("done!")
 
 if __name__ == '__main__':
