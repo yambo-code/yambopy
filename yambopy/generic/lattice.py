39f15239a14e91c58d1709ecf6017c4315aac8fd
Henrique Miranda <miranda.henrique@gmail.com>
2017-11-04 09:15:21 +0100

    Converted lattice.py from python 2 to 3

--- yambopy/generic/lattice.py
+++ yambopy/generic/lattice.py
@@ -29,13 +29,13 @@ def red_car(red,lat):
     Convert reduced coordinates to cartesian
     """
     lat = np.array(lat)
-    return np.array(map( lambda coord: coord[0]*lat[0]+coord[1]*lat[1]+coord[2]*lat[2], red))
+    return np.array([coord[0]*lat[0]+coord[1]*lat[1]+coord[2]*lat[2] for coord in red])
 
 def car_red(car,lat):
     """
     Convert cartesian coordinates to reduced
     """
-    return np.array(map( lambda coord: np.linalg.solve(np.array(lat).T,coord), np.array(car)))
+    return np.array([np.linalg.solve(np.array(lat).T,coord) for coord in np.array(car)])
 
 def isbetween(a,b,c):
     #check if c is between a and b
@@ -95,9 +95,9 @@ class Lattice():
             weights_ibz[nk_ibz] = float(inv_weight)/nkpoints
 
         #irreducible kpoints
-        irr_kpoints  = (grid[counter.keys()] + [0.0, 0.0, 0.0]) / kmesh
+        irr_kpoints  = (grid[list(counter.keys())] + [0.0, 0.0, 0.0]) / kmesh
         full_kpoints = (grid + [0.0, 0.0, 0.0]) / kmesh
-        mapping_to_ibz = dict(zip(counter.keys(),xrange(nkpoints_ibz)))
+        mapping_to_ibz = dict(list(zip(list(counter.keys()),list(range(nkpoints_ibz)))))
         ibz_to_full = [mapping_to_ibz[i] for i in mapping]
     
         self.red_kpoints = full_kpoints
@@ -121,19 +121,19 @@ class Lattice():
         lat = lat*np.array([1.0/nx,1.0/ny,1.0/nz])
         
         #generate new wigner seitz
-        xiter = xrange(-nx,nx)
-        yiter = xrange(-ny,ny)
-        ziter = xrange(-nz,nz)
-        if nx == 1: xiter = xrange(1)
-        if ny == 1: yiter = xrange(1)
-        if nz == 1: ziter = xrange(1)
+        xiter = range(-nx,nx)
+        yiter = range(-ny,ny)
+        ziter = range(-nz,nz)
+        if nx == 1: xiter = range(1)
+        if ny == 1: yiter = range(1)
+        if nz == 1: ziter = range(1)
                     
         points = np.array([(x,y,z) for x,y,z in product(xiter,yiter,ziter)])
         points = red_car(points,lat)
 
         #create a list of 26 points corresponding to the neighbouring cells
         neighbours = []
-        for x,y,z in product(xrange(-1,2),repeat=3):
+        for x,y,z in product(range(-1,2),repeat=3):
             #do not add zero
             if x==0 and y==0 and x==0:
                 continue
@@ -207,7 +207,7 @@ class Lattice():
             end_kpt   = path_car[k+1] #end point of the path
 
             #generate repetitions of the brillouin zone
-            for x,y,z in product(range(-1,2),range(-1,2),range(1)):
+            for x,y,z in product(list(range(-1,2)),list(range(-1,2)),list(range(1))):
 
                 #shift the brillouin zone
                 shift = red_car([np.array([x,y,z])],self.rlat)[0]
@@ -224,13 +224,13 @@ class Lattice():
                         kpoints_in_path[key] = value
 
             #sort the points acoording to distance to the start of the path
-            kpoints_in_path = sorted(kpoints_in_path.values(),key=lambda i: i[1])
+            kpoints_in_path = sorted(list(kpoints_in_path.values()),key=lambda i: i[1])
 
             #for all the kpoints in the path
             for index, disp, kpt in kpoints_in_path:
                 bands_kpoints.append( kpt )
                 bands_indexes.append( index )
-                print ("%12.8lf "*3)%tuple(kpt), index
+                print(("%12.8lf "*3)%tuple(kpt), index)
 
         self.bands_kpoints = bands_kpoints
         self.bands_indexes = bands_indexes
@@ -244,16 +244,16 @@ class Lattice():
             Arguments:
             lat - lattice instance with the double grid
             """
-            print "double grid:", lattice.full_nkpoints
-            print "coarse grid:", self.full_nkpoints
+            print("double grid:", lattice.full_nkpoints)
+            print("coarse grid:", self.full_nkpoints)
             
             #repeat the coarse kpoints in three directions
             coarse_kpoints = []
             coarse_indexes = []
-            for i,j,k in itertools.product(xrange(-1,2),repeat=3):
+            for i,j,k in itertools.product(range(-1,2),repeat=3):
                 disp = red_car([[i,j,k]],self.rlat)[0]
                 coarse_kpoints.append( self.car_kpoints + disp )
-                coarse_indexes.append( xrange(self.full_nkpoints) )
+                coarse_indexes.append( range(self.full_nkpoints) )
                 
             #shift the coarse kpoints by a infinitesimal
             coarse_kpoints = np.vstack(coarse_kpoints)+delta_shift
@@ -272,9 +272,9 @@ class Lattice():
             
             delta_shift = np.array(delta_shift)    
             double_to_coarse_grid = [0]*lattice.full_nkpoints                   #many to one
-            coarse_to_double_grid = [ [] for nk in xrange(self.full_nkpoints) ] #one to many
+            coarse_to_double_grid = [ [] for nk in range(self.full_nkpoints) ] #one to many
 
-            print "finding neighbour k-points:"
+            print("finding neighbour k-points:")
             start_time = time.time()
         
             #iterate over the points of the double grid
@@ -288,7 +288,7 @@ class Lattice():
                 double_to_coarse_grid[dg_nk] = coarse_nk
                 coarse_to_double_grid[coarse_nk].append(dg_nk)
                 
-            print "took %4.2lfs" % (time.time()-start_time)
+            print("took %4.2lfs" % (time.time()-start_time))
             return double_to_coarse_grid, coarse_to_double_grid
                     
     def __str__(self):
