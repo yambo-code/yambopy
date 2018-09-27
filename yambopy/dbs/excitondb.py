Cleanup of the LatticeDB and SaveDB routines. Added references for BSE. Fixed units in the calculation of chi with excitonic effects.

yambopy/dbs/excitondb.py
@@ -60,10 +60,10 @@ class YamboExcitonDB(YamboSaveDB):
         self.eigenvectors = eigenvectors
 
     @classmethod
-    def from_db_file(cls,lattice,filename='ndb.BS_diago_Q01',path='.'):
+    def from_db_file(cls,lattice,filename='ndb.BS_diago_Q01',folder='.'):
         """ initialize this class from a file
         """
-        path_filename = os.path.join(path,filename)
+        path_filename = os.path.join(folder,filename)
         if not os.path.isfile(path_filename):
             raise FileNotFoundError("File %s not found in YamboExcitonDB"%path_filename)
 
@@ -333,12 +333,13 @@ class YamboExcitonDB(YamboSaveDB):
         """
         if self.eigenvectors is None:
             raise ValueError('This database does not contain Excitonic states,'
-                              'please re-run the yambo BSE calculation with the WRbsWF option in the input file.')
+                             'please re-run the yambo BSE calculation with the WRbsWF option in the input file.')
         if isinstance(excitons, int):
             excitons = (excitons,)
        
         car_kpoints = self.lattice.car_kpoints
         nkpoints = len(car_kpoints)
+        print(nkpoints)
         amplitudes = np.zeros([nkpoints])
         phases     = np.zeros([nkpoints],dtype=np.complex64)
         for exciton in excitons:
@@ -358,7 +359,8 @@ class YamboExcitonDB(YamboSaveDB):
 
         return car_kpoints, amplitudes[kindx], np.angle(phases)[kindx]
 
-    def chi(self,dipoles=None,dir=0,emin=0,emax=8,estep=0.01,broad=0.1,nexcitons='all'):
+    def chi(self,dipoles=None,dir=0,emin=0,emax=10,estep=0.02,broad=0.1,q0norm=1e-5,
+            nexcitons='all',spin_degen=2,verbose=0):
         """
         Calculate the dielectric response function using excitonic states
         """
@@ -380,9 +382,8 @@ class YamboExcitonDB(YamboSaveDB):
             EL2 = self.r_residual
         else:
             #calculate exciton-light coupling
-            print("calculate exciton-light coupling")
+            if verbose: print("calculate exciton-light coupling")
             EL1,EL2 = self.project1(dipoles.dipoles[:,dir],nexcitons) 
-
 
         #iterate over the excitonic states
         for s in range(nexcitons):
@@ -390,11 +391,16 @@ class YamboExcitonDB(YamboSaveDB):
             es = self.eigenvalues[s]
  
             #calculate the green's functions
-            G1 = 1/(   w - es - broad*I)
-            G2 = 1/( - w - es - broad*I)
+            G1 = -1/(   w - es + broad*I)
+            G2 = -1/( - w - es - broad*I)
 
             r = EL1[s]*EL2[s]
             chi += r*G1 + r*G2
+
+        #multiply facto
+        d3k_factor = self.lattice.rlat_vol/self.lattice.nkpoints
+        cofactor = spin_degen/(2*np.pi)**3 * d3k_factor * (4*np.pi)
+        chi = chi*cofactor/q0norm**2
 
         return w,chi
 
@@ -402,7 +408,7 @@ class YamboExcitonDB(YamboSaveDB):
         lines = []; app = lines.append
         app( marquee(self.__class__.__name__,mark=mark) )
         app( "number of excitons:    %d"%self.nexcitons )
-        if self.eigenvectors is not None: app( "number of transitions: %d"%self.ntransitions )
+        if self.table is not None: app( "number of transitions: %d"%self.ntransitions )
         return '\n'.join(lines)
     
     def __str__(self):

yambopy/dbs/latticedb.py
@@ -3,11 +3,12 @@
 #
 # This file is part of the yambopy project
 #
-from itertools import product, chain
+import os
 import numpy as np
+from itertools import product
 from netCDF4 import Dataset
 from yambopy.tools.jsonencoder import JsonDumper, JsonLoader
-from yambopy.lattice import rec_lat, car_red, red_car, vec_in_list, isbetween
+from yambopy.lattice import vol_lat, rec_lat, car_red, red_car, vec_in_list, isbetween
 from yambopy.units import atomic_mass
 from yambopy.tools.string import marquee
 from qepy.lattice import Path
@@ -18,20 +19,38 @@ class YamboLatticeDB():
     """
     def __init__(self,lat=None,alat=None,sym_car=None,iku_kpoints=None,
                       atomic_positions=None,atomic_numbers=None,time_rev=None):
-        self.lat = np.array(lat)
-        self.alat = np.array(alat)
-        self.sym_car = np.array(sym_car)
-        self.iku_kpoints = np.array(iku_kpoints)
+        self.lat              = np.array(lat)
+        self.alat             = np.array(alat)
+        self.sym_car          = np.array(sym_car)
+        self.iku_kpoints      = np.array(iku_kpoints)
         self.atomic_positions = np.array(atomic_positions)
-        self.atomic_numbers = np.array(atomic_numbers)
-        self.time_rev = time_rev
+        self.atomic_numbers   = np.array(atomic_numbers)
+        self.time_rev         = time_rev
 
     @classmethod
     def from_db_file(cls,filename):
         """ Initialize YamboLattice from a local dbfile """
-        y = cls()
-        y.read_db(filename)
-        y._process()
+
+        if not os.path.isfile(filename):
+            raise FileNotFoundError("error opening %s in YamboLatticeDB"%filename)
+
+        with Dataset(filename) as database:
+
+            dimensions = database.variables['DIMENSIONS'][:]
+
+            natoms = database.variables['N_ATOMS'][:].astype(int).T
+            tmp_an = database.variables['atomic_numbers'][:].astype(int)
+            flatten = lambda l: [item for sublist in l for item in sublist]
+
+            args = dict( atomic_numbers   = flatten([[tmp_an[n]]*na for n,na in enumerate(natoms)]),
+                         atomic_positions = database.variables['ATOM_POS'][0,:],
+                         sym_car          = database.variables['SYMMETRY'][:],
+                         iku_kpoints      = database.variables['K-POINTS'][:].T,
+                         lat              = database.variables['LATTICE_VECTORS'][:].T,
+                         alat             = database.variables['LATTICE_PARAMETER'][:].T,
+                         time_rev         = dimensions[9] )
+
+        y = cls(**args)
         y.expand_kpoints()
         return y
  
@@ -46,9 +65,8 @@ class YamboLatticeDB():
         atomic_positions = data["atomic_positions"]
         atomic_numbers = data["atomic_numbers"]
         time_rev = data["time_rev"]
+
         y = cls(lat,alat,sym_car,iku_kpoints,atomic_positions,atomic_numbers,time_rev)
-        y._process()
-        y.expand_kpoints()
         return y
 
     @classmethod
@@ -59,34 +77,6 @@ class YamboLatticeDB():
     @property
     def nkpoints(self):
         return len(self.car_kpoints)
-
-    def read_db(self,filename):
-        try:
-            database = Dataset(filename)
-        except:
-            raise IOError("error opening %s in YamboLatticeDB"%filename)
-
-        #lattice data
-        self.lat         = database.variables['LATTICE_VECTORS'][:].T
-        self.alat        = database.variables['LATTICE_PARAMETER'][:].T
-        self.sym_car     = database.variables['SYMMETRY'][:]
-        self.iku_kpoints = database.variables['K-POINTS'][:].T
-
-        #atomic numbers
-        natoms         = database.variables['N_ATOMS'][:].astype(int).T
-        self.atomic_positions = database.variables['ATOM_POS'][:,0,:]
-        atomic_numbers = database.variables['atomic_numbers'][:].astype(int)
-        atomic_numbers = [[atomic_numbers[n]]*na for n,na in enumerate(natoms)]
-        self.atomic_numbers = list(chain.from_iterable(atomic_numbers))
-        self.atomic_masses = [atomic_mass[a] for a in self.atomic_numbers]
-        
-        dimensions = database.variables['DIMENSIONS'][:]
-        self.temperature = dimensions[13]
-        self.nelectrons = dimensions[14]
-        self.spin = int(dimensions[11])
-        self.time_rev = dimensions[9]
-
-        database.close()
 
     def as_dict(self):
         """ get the information of this class as a dictionary
@@ -104,29 +94,68 @@ class YamboLatticeDB():
         """ write a json file with the lattice information """
         JsonDumper(self.as_dict(),filename)
 
-    def _process(self):
-        """
-        Generate additional information from data in the database
-        """
-        inv = np.linalg.inv
-        #caclulate the reciprocal lattice
-        self.rlat  = rec_lat(self.lat)
-        self.nsym  = len(self.sym_car)
+    @property
+    def iku_kpoints(self):
+        return self._iku_kpoints
 
-        #convert form internal yambo units to cartesian lattice units
-        self.car_kpoints = np.array([ k/self.alat for k in self.iku_kpoints ])
-        self.red_kpoints = car_red(self.car_kpoints,self.rlat)
+    @iku_kpoints.setter
+    def iku_kpoints(self,value):
+        if hasattr(self,"_red_kpoints"): delattr(self,"_red_kpoints")
+        if hasattr(self,"_car_kpoints"): delattr(self,"_car_kpoints")
+        self._iku_kpoints = value
 
-        #convert cartesian transformations to reciprocal transformations
-        self.sym_rec = np.zeros([self.nsym,3,3])
+    @property
+    def nkpoints(self):
+        return len(self.iku_kpoints)
+
+    @property
+    def nsym(self):
+        return len(self.sym_car)
+
+    @property
+    def rlat(self):
+        """caclulate the reciprocal lattice"""
+        if not hasattr(self,'_rlat'):
+            self._rlat = rec_lat(self.lat)
+        return self._rlat 
+
+    @property
+    def rlat_vol(self):
+        return (2*np.pi)**3 * vol_lat(self.rlat)
+
+    @property
+    def lat_vol(self):
+        return vol_lat(self.lat)
+
+    @property
+    def car_kpoints(self):
+        """convert form internal yambo units to cartesian lattice units"""
+        if not hasattr(self,"_car_kpoints"):
+            self._car_kpoints = np.array([ k/self.alat for k in self.iku_kpoints ])
+        return self._car_kpoints
+
+    @property
+    def red_kpoints(self):
+        """convert from cartesian coordinates to reduced coordinates"""
+        if not hasattr(self,"_red_kpoints"):
+            self._red_kpoints = car_red(self.car_kpoints,self.rlat)
+        return self._red_kpoints
+    
+    @property
+    def sym_rec(self):
+        """Convert cartesian transformations to reciprocal transformations"""
+        sym_rec = np.zeros([self.nsym,3,3])
         for n,s in enumerate(self.sym_car):
-            self.sym_rec[n] = inv(s).T
+            sym_rec[n] = np.linalg.inv(s).T
+        return sym_rec
 
-        #get a list of symmetries with time reversal
-        nsym = len(self.sym_car)
-        self.time_rev_list = [False]*nsym
-        for i in range(nsym):
-            self.time_rev_list[i] = ( i >= nsym/(self.time_rev+1) )
+    @property
+    def time_rev_list(self):
+        """get a list of symmetries with time reversal"""
+        time_rev_list = [False]*self.nsym
+        for i in range(self.nsym):
+            time_rev_list[i] = ( i >= self.nsym/(self.time_rev+1) )
+        return time_rev_list
 
     def expand_kpoints(self,atol=1e-6,verbose=0):
         """
@@ -165,19 +194,18 @@ class YamboLatticeDB():
                     continue
 
         #calculate the weights of each of the kpoints in the irreducible brillouin zone
-        self.full_nkpoints = len(kpoints_full)
-        weights = np.zeros([self.nkpoints])
+        nkpoints_full = len(kpoints_full)
+        weights = np.zeros([nkpoints_full])
         for nk in kpoints_full_i:
-            weights[nk] = float(len(kpoints_full_i[nk]))/self.full_nkpoints
+            weights[nk] = float(len(kpoints_full_i[nk]))/nkpoints_full
 
         if verbose: print("%d kpoints expanded to %d"%(len(self.car_kpoints),len(kpoints_full)))
 
         #set the variables
         self.weights_ibz      = np.array(weights)
-        self.car_kpoints      = np.array(kpoints_full)
-        self.red_kpoints      = car_red(self.car_kpoints,self.rlat)
         self.kpoints_indexes  = np.array(kpoints_indexes)
         self.symmetry_indexes = np.array(symmetry_indexes)
+        self.iku_kpoints      = [k*self.alat for k in kpoints_full]
 
     def get_path(self,path,debug=False):
         """
@@ -185,9 +213,6 @@ class YamboLatticeDB():
         """
         if isinstance(path,Path):
             path = path.get_klist()
-
-        nks  = list(range(self.nkpoints))
-        kpts = self.car_kpoints
 
         #points in cartesian coordinates
         path_car = red_car(path, self.rlat)
@@ -217,7 +242,7 @@ class YamboLatticeDB():
                 shift = red_car([np.array([x,y,z])],self.rlat)[0]
 
                 #iterate over all the kpoints
-                for index, kpt in zip(nks,kpts):
+                for index, kpt in enumerate(self.car_kpoints):
 
                     kpt_shift = kpt+shift #shift the kpoint
 
@@ -245,9 +270,10 @@ class YamboLatticeDB():
     def __str__(self):
         lines = []; app = lines.append
         app(marquee(self.__class__.__name__))
-        app("\nlattice:")
+        app("lattice:")
         lines += [("%12.8lf " * 3) % tuple(vec) for vec in self.lat]
-        app("\natom positions:")
+        app("atom positions:")
+        print(self.atomic_numbers)
         for an, pos in zip(self.atomic_numbers, self.atomic_positions):
             app( "%3d " % an + ("%12.8lf " * 3) % tuple(pos) )
         return "\n".join(lines)

yambopy/dbs/savedb.py
@@ -3,10 +3,11 @@
 #
 # This file is part of the yambopy project
 #
-from yambopy import *
-from yambopy.plot import *
+import os
+import numpy as np
 from itertools import product
 from netCDF4 import Dataset
+from yambopy.plot import *
 from yambopy.lattice import isbetween, car_red, rec_lat
 from yambopy.units import ha2ev
 
@@ -49,83 +50,135 @@ class YamboSaveDB():
         ``eigenvalues`` : eigenvalues of the electrons in eV
         ``nkpoints`` : number of kpoints
     """
-    def __init__(self,save='SAVE',filename='ns.db1'):
-        self.save = save
-        self.filename = filename
-        path_filename = '%s/%s'%(save,filename)
+    def __init__(self,atomic_numbers,atomic_positions,eigenvalues,sym_car,kpts_iku,
+                 lat,alat,temperature,electrons,spin,time_rev):
 
-        #read the database
-        self.readDB(path_filename)
+        self.atomic_numbers   = atomic_numbers   
+        self.atomic_positions = atomic_positions
+        self.eigenvalues      = eigenvalues     
+        self.sym_car          = sym_car         
+        self.kpts_iku         = kpts_iku        
+        self.lat              = lat             
+        self.alat             = alat            
+        self.temperature      = temperature     
+        self.electrons        = electrons       
+        self.spin             = spin            
+        self.time_rev         = time_rev        
 
-        #status
+        #TODO: remove this
         self.expanded = False
-        self.efermi = None
 
-    def readDB(self,filename):
+    @classmethod
+    def from_db_file(cls,folder='.',filename='ns.db1'):
         """
         Read the ns.db1 database
         """
-        try:
-            database    = Dataset(filename)
-        except:
-            raise ValueError( "Error reading %s database in YamboSaveDB"%filename )
+        path_filename = os.path.join(folder,filename)
+        if not os.path.isfile(path_filename):
+            raise FileNotFoundError( "Error reading %s database in YamboSaveDB"%path_filename )
 
-        self.atomic_numbers   = database.variables['atomic_numbers'][:]
-        self.atomic_positions = database.variables['ATOM_POS'][0,:]
-        # we convert the eigenvalues to eV
-        self.eigenvalues      = database.variables['EIGENVALUES'][0,:]*ha2ev
-        self.sym_car          = database.variables['SYMMETRY'][:]
-        self.kpts_iku         = database.variables['K-POINTS'][:].T
-        self.lat              = database.variables['LATTICE_VECTORS'][:].T
-        self.alat             = database.variables['LATTICE_PARAMETER'][:].T
-        dimensions = database.variables['DIMENSIONS'][:]
-        self.temperature = dimensions[13]
-        self.electrons = dimensions[14]
-        self.nkpoints  = int(dimensions[6])
-        self.spin = int(dimensions[11])
-        self.time_rev = dimensions[9]
-        database.close()
+        with Dataset(path_filename) as database:
+            
+            dimensions            = database.variables['DIMENSIONS'][:]
+            
+            natoms = database.variables['N_ATOMS'][:].astype(int).T
+            tmp_an = database.variables['atomic_numbers'][:].astype(int)
 
-        self.natoms = len(self.atomic_positions)
-        _,self.nbands = self.eigenvalues.shape
+            args = dict( atomic_numbers   = [[tmp_an[n]]*na for n,na in enumerate(natoms)],
+                         atomic_positions = database.variables['ATOM_POS'][0,:],
+                         eigenvalues      = database.variables['EIGENVALUES'][0,:]*ha2ev,
+                         sym_car          = database.variables['SYMMETRY'][:],
+                         kpts_iku         = database.variables['K-POINTS'][:].T,
+                         lat              = database.variables['LATTICE_VECTORS'][:].T,
+                         alat             = database.variables['LATTICE_PARAMETER'][:].T,
+                         temperature      = dimensions[13],
+                         electrons        = dimensions[14],
+                         spin             = int(dimensions[11]),
+                         time_rev         = dimensions[9] )
 
-        #get a list of symmetries with time reversal
-        nsym = len(self.sym_car)
-        self.time_rev_list = [False]*nsym
-        for i in range(nsym):
-            self.time_rev_list[i] = ( i >= nsym/(self.time_rev+1) )
+        return cls(**args)
 
-        #spin degeneracy if 2 components degen 1 else degen 2
-        self.spin_degen = [0,2,1][int(self.spin)]
+    @property
+    def spin_degen(self):
+        """spin degeneracy if 2 components degen 1 else degen 2"""
+        return [0,2,1][int(self.spin)]
 
-        #get minimum am maximul energies
-        eiv = self.eigenvalues.flatten()
-        self.min_eival = min(eiv)
-        self.max_eival = max(eiv)
+    @property
+    def min_eival(self):
+        return np.min(self.eigenvalues) 
+    
+    @property
+    def max_eival(self):
+        return np.max(self.eigenvalues)
 
-        #caclulate the reciprocal lattice
-        self.rlat  = rec_lat(self.lat)
-        self.nsym  = len(self.sym_car)
+    @property
+    def car_kpoints(self):
+        """convert form internal yambo units to cartesian lattice units"""
+        return np.array([ k/self.alat for k in self.kpts_iku ])
 
-        #convert form internal yambo units to cartesian lattice units
-        self.kpts_car = np.array([ k/self.alat for k in self.kpts_iku ])
+    @property
+    def red_kpoints(self):
+        """convert from cartesian coordinates to reduced coordinates"""
+        if not hasattr(self,"_red_kpoints"):
+            self._red_kpoints = car_red(self.car_kpoints,self.rlat)
+        return self._red_kpoints
 
-        #convert cartesian transformations to reduced transformations
-        inv = np.linalg.inv
-        self.sym_rlu = np.zeros([self.nsym,3,3])
+    @property
+    def rlat(self):
+        """caclulate the reciprocal lattice"""
+        return rec_lat(self.lat)
+
+    @property
+    def natoms(self):
+        return len(self.atomic_positions)
+
+    @property
+    def nbands(self):
+        _,nbands = self.eigenvalues.shape
+        return nbands
+
+    @property
+    def nkpoints(self):
+        return len(self.kpts_iku)
+
+    @property
+    def time_rev_list(self):
+        """get a list of symmetries with time reversal"""
+        time_rev_list = [False]*self.nsym
+        for i in range(self.nsym):
+            time_rev_list[i] = ( i >= self.nsym/(self.time_rev+1) )
+        return time_rev_list
+
+    @property
+    def sym_rlu(self):
+        """convert cartesian transformations to reduced transformations """
+        sym_rlu = np.zeros([self.nsym,3,3])
         for n,s in enumerate(self.sym_car):
             a = np.dot(s.T,inv(self.rlat))
-            self.sym_rlu[n] = np.dot(inv(self.lat.T),a)
+            sym_rlu[n] = np.dot(inv(self.lat.T),a)
+        return sym_rlu
 
-        #convert cartesian transformations to reciprocal transformations
-        self.sym_rec = np.zeros([self.nsym,3,3])
+    @property
+    def nsym(self):
+        return len(self.sym_car)
+
+    @property
+    def sym_rec(self):
+        """Convert cartesian transformations to reciprocal transformations"""
+        sym_rec = np.zeros([self.nsym,3,3])
         for n,s in enumerate(self.sym_car):
-            self.sym_rec[n] = inv(s).T
+            sym_rec[n] = np.linalg.inv(s).T
+        return sym_rec
 
-    def get_fermi(self,inv_smear=0.001):
+    @property
+    def efermi(self):
+        if not hasattr(self,"_efermi"):
+            self._efermi = self.get_efermi
+        return self._efermi
+
+    def get_fermi(self,inv_smear=0.001,verbose=0):
         """ Determine the fermi energy
         """
-        if self.efermi: return self.efermi
         from scipy.optimize import bisect
 
         kpts, nks, nss = self.expand_kpts()
@@ -150,19 +203,17 @@ class YamboSaveDB():
             """
             return sum([sum(self.spin_degen*fermi_array(self.eigenvalues[nk],ef))*self.weights[nk] for nk in range(self.nkpoints)])-self.electrons
 
-        self.efermi = bisect(occupation_minus_ne,self.min_eival,self.max_eival)
+        efermi = bisect(occupation_minus_ne,self.min_eival,self.max_eival)
 
-        print("fermi: %lf eV"%self.efermi)
+        if verbose: print("fermi: %lf eV"%efermi)
 
-        self.eigenvalues -= self.efermi
-        self.min_eival -= self.efermi
-        self.max_eival -= self.efermi
+        self.eigenvalues -= efermi
 
         self.occupations = np.zeros([self.nkpoints,self.nbands],dtype=np.float32)
         for nk in range(self.nkpoints):
             self.occupations[nk] = fermi_array(self.eigenvalues[nk,:self.nbands],0)
 
-        return self.efermi
+        return efermi
 
     def write_kpoints(self,filename_full='kpts_full.dat',filename='kpts.dat'):
         """ Write the kpoints in a file
@@ -175,7 +226,7 @@ class YamboSaveDB():
         f.close()
 
         f = open(filename,'w')
-        for k in self.kpts_car:
+        for k in self.car_kpoints:
             f.write(("%12.8lf "*3)%tuple(k)+"\n")
         f.close()
 
@@ -256,7 +307,7 @@ class YamboSaveDB():
         kpoints_full_i = {}
 
         #expand using symmetries
-        for nk,k in enumerate(self.kpts_car):
+        for nk,k in enumerate(self.car_kpoints):
             for ns,sym in enumerate(self.sym_car):
                 new_k = np.dot(sym,k)
 
@@ -288,7 +339,7 @@ class YamboSaveDB():
         self.kpoints_indexes  = np.array(kpoints_indexes)
         self.symmetry_indexes = np.array(symmetry_indexes)
 
-        print("%d kpoints expanded to %d"%(len(self.kpts_car),len(kpoints_full)))
+        print("%d kpoints expanded to %d"%(len(self.car_kpoints),len(kpoints_full)))
 
         return self.kpoints_full, self.kpoints_indexes, self.symmetry_indexes
 
@@ -334,7 +385,7 @@ class YamboSaveDB():
             kpts, nks = self.expand_kpts(repx=repx,repy=repy,repz=repz)
             weights = weights[nks]
         else:
-            kpts = self.kpts_car
+            kpts = self.car_kpoints
 
         fig = plt.figure(figsize=(10,10))
         plt.scatter(kpts[:,0], kpts[:,1], s=size, marker='H', cmap=cmap, lw=0, c=weights)

yambopy/dbs/tests/test_excitondb.py
@@ -3,6 +3,7 @@
 #
 # This file is part of yambopy
 #
+import numpy as np
 import unittest
 import os
 from yambopy.dbs.excitondb import YamboExcitonDB
@@ -23,7 +24,7 @@ class TestYamboExcitonDB(unittest.TestCase):
 
         #load databases
         lat  = YamboLatticeDB.from_db_file(os.path.join(test_path,'SAVE','ns.db1'))
-        exc  = YamboExcitonDB(lat,path=os.path.join(test_path,'yambo'))
+        exc  = YamboExcitonDB.from_db_file(lat,folder=os.path.join(test_path,'yambo'))
         electrons = YamboElectronsDB(lat,save=os.path.join(test_path,'SAVE')) 
 
         #show output
@@ -33,6 +34,13 @@ class TestYamboExcitonDB(unittest.TestCase):
 
         #write exc_I.dat and exc_E.dat
         exc.write_sorted('exc')
+
+        #calculate chi
+        w,chi = exc.chi()
+
+        #load reference
+        eps_filename = os.path.join(test_path,'o-yambo.eps_q1_diago_bse')
+        w_ref,chi_imag_ref,chi_real_ref = np.loadtxt(eps_filename,unpack=True)[:3]
 
         #get amplitude and phases
         kpoints, amplitude, phase = exc.get_amplitudes_phases((1,0,))

yambopy/lattice.py
@@ -55,12 +55,19 @@ def car_red(car,lat):
     """
     return np.array([np.linalg.solve(np.array(lat).T,coord) for coord in car])
 
+def vol_lat(lat):
+    """
+    Calculate the volume of a lattice
+    """
+    a1,a2,a3 = np.array(lat)
+    return np.dot(a1,np.cross(a2,a3))
+
 def rec_lat(lat):
     """
     Calculate the reciprocal lattice vectors
     """
+    v = vol_lat(lat)
     a1,a2,a3 = np.array(lat)
-    v = np.dot(a1,np.cross(a2,a3))
     b1 = np.cross(a2,a3)/v
     b2 = np.cross(a3,a1)/v
     b3 = np.cross(a1,a2)/v