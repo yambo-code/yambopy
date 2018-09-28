93a82f42bb8fe30a08eb231ef123e60dfa73dc24
Henrique Miranda <miranda.henrique@gmail.com>
2018-09-28 11:07:43 +0100

    Update QE and Yambo version on Travis.

--- tests/install.sh
+++ tests/install.sh
@@ -6,8 +6,10 @@ set -x # print to stdout
 echo "Yambo: $YAMBO_VERSION"
 echo "Quantum Espresso: $PW_VERSION"
 
-wget https://www.dropbox.com/s/hna9v4yw6u7ea5e/yambo_4.1.tar?dl=0 -O yambo.tar # Yambo 4.1 tar
+# Yambo 4.4 tar
+wget https://www.dropbox.com/s/7w8f0ll2zq6rwzy/yambo_4.4.tar?dl=0 -O yambo.tar
 sudo tar -xf yambo.tar -C /bin/
 
-wget https://www.dropbox.com/s/o91hx046chfk26d/espresso_5.4.tar?dl=0 -O espresso.tar # Espresso 5.4 tar
+# Espresso 6.3 tar
+wget https://www.dropbox.com/s/lv82dwp434sud6d/espresso_6.3.tar?dl=0 -O espresso.tar
 sudo tar -xf espresso.tar -C /bin/
