398c434f74008c1ad5b629f8c30624e9ce6c94e5
Henrique Miranda <miranda.henrique@gmail.com>
2018-09-28 12:46:55 +0100

    Compiled yambo and qe binaries in the correct environment

--- tests/install.sh
+++ tests/install.sh
@@ -9,9 +9,9 @@ echo "Quantum Espresso: $PW_VERSION"
 ls /usr/lib
 
 # Yambo 4.4 tar
-wget https://www.dropbox.com/s/7w8f0ll2zq6rwzy/yambo_4.4.tar?dl=0 -O yambo.tar
+wget https://www.dropbox.com/s/2eyiuta4sta0vmm/yambo_4.4.tar?dl=0 -O yambo.tar
 sudo tar -xf yambo.tar -C /bin/
 
 # Espresso 6.3 tar
-wget https://www.dropbox.com/s/lv82dwp434sud6d/espresso_6.3.tar?dl=0 -O espresso.tar
+wget https://www.dropbox.com/s/ltxv5nzv4z4refx/espresso_6.3.tar?dl=0 -O espresso.tar
 sudo tar -xf espresso.tar -C /bin/
