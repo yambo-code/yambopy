656dfbdc3959bb1b54b8761d8e5b09d55ad72d2a
Henrique Miranda <miranda.henrique@gmail.com>
2018-09-28 21:42:25 +0100

    Added missing banner in th factories file.

--- yambopy/io/factories.py
+++ yambopy/io/factories.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2018 Henrique Pereira Coutada Miranda
+# All rights reserved.
+#
+# This file is part of yambopy
+#
 """
 This file contains classes and methods to generate input files
 """
