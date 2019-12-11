6a2819effe13d3a15916517ff267e1660b3eea44
Fulvio Paleari <fulvio.paleari90@gmail.com>
2019-12-11 16:08:26 +0100

    Saving data in order to checkout

--- tutorial/bn/gw_pp_bn.py
+++ tutorial/bn/gw_pp_bn.py
@@ -7,15 +7,25 @@
 # These example runs locally, if you want to make it work behind a queing system you need to
 # modify it accordingly
 #
-from __future__ import print_function, import division
+#from __future__ import print_function, import division
 from yambopy import *
 from qepy import *
+from schedulerpy import *
+import argparse
+
+#parse options
+parser = argparse.ArgumentParser(description='Test the yambopy script.')
+parser.add_argument('-c', '--calc', action="store_true", help='calculate the manually parallelised GW')
+parser.add_argument('-p', '--plot', action="store_true", help='plot the results')
+args = parser.parse_args()
+
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
 
 yambo =  'yambo'
 folder = 'gw_pp_par'
-
-if not os.path.isdir('database'):
-    os.mkdir('database')
+scheduler = Scheduler.factory
 
 #check if the nscf cycle is present
 if os.path.isdir('nscf/bn.save'):
@@ -27,71 +37,75 @@ else:
 #check if the SAVE folder is present
 if not os.path.isdir('database/SAVE'):
     print('preparing yambo database')
-    os.system('cd nscf/bn.save; p2y')
-    os.system('cd nscf/bn.save; yambo')
-    os.system('mv nscf/bn.save/SAVE database')
+    p2y_run = scheduler()
+    p2y_run.add_command('mkdir -p database')
+    p2y_run.add_command('cd nscf/bn.save; p2y > p2y.log')
+    p2y_run.add_command('yambo > yambo.log')
+    p2y_run.add_command('mv SAVE ../../database/')
+    p2y_run.run()
 
-if not os.path.isdir(folder):
-    os.mkdir(folder)
-    os.system('cp -r database/SAVE %s'%folder)
+if not os.path.isdir('%s/SAVE'%folder):
+    s = scheduler()
+    s.add_command('mkdir -p %s'%folder)
+    s.add_command('cd %s; cp -r ../database/SAVE .'%folder)
+    s.run()
 
-#create the yambo input file
-y = YamboIn('%s -p p -V all'%yambo,folder=folder)
-_,nqpoints = y['QpntsRXp'][0] 
+if args.calc:
+    #create the yambo input file for dipoles and plasmon pole
+    y = YamboIn.from_runlevel('%s -p p -V all'%yambo,folder=folder)
+    _,nqpoints = y['QpntsRXp'][0] 
 
+    y['FFTGvecs'] = [15,'Ry']
+    y['NGsBlkXp'] = [1,'Ry']
+    y['BndsRnXp'] = [[1,30],'']
 
-y['FFTGvecs'] = [15,'Ry']
-y['NGsBlkXp'] = [1,'Ry']
-y['BndsRnXp'] = [[1,30],'']
+    #prepare the q-points input files
+    for nq in range(1,int(nqpoints)+1):
+        y['QpntsRXp'] = [[nq,nq],'']
+        y.write('%s/yambo_q%d.in'%(folder,nq))
+    #calculate first q-point and dipoles
+    shell = scheduler()
+    shell.add_command('cd %s; %s -F yambo_q1.in -J 1'%(folder,yambo))
+    shell.add_command('mkdir -p yambo')
+    #gather the dipoles in save
+    shell.add_command('cp 1/ndb.dipoles* SAVE') 
+    shell.add_command('cp 1/ndb.pp SAVE')
+    #run all
+    print('running separate yambo files')
+    for nq in range(2,int(nqpoints)+1): shell.add_command('%s -F yambo_q%s.in -J %s'%(yambo,str(nq),str(nq)))
+    shell.run()
 
-#prepare the q-points input files
-f = open('jobs.sh','w')
-for nq in range(1,int(nqpoints)+1):
-    y['QpntsRXp'] = [[nq,nq],'']
-    y.write('%s/yambo_q%d.in'%(folder,nq))
-    if nq != 1:
-        f.write('cd %s; %s -F yambo_q%d.in -J %d\n'%(folder,yambo,nq,nq))
-f.close()
+    #create the yambo input file for gw
+    y = YamboIn.from_runlevel('yambo -p p -g n -V all',folder=folder)
+    QPKrange,_ = y['QPkrange']
+    y['QPkrange'] = [QPKrange[:2]+[3,6],'']
+    y['FFTGvecs'] = [15,'Ry']
+    y['NGsBlkXp'] = [1,'Ry']
+    y['BndsRnXp'] = [[1,30],'']
+    y.write('%s/yambo_run.in'%folder)
 
-#calculate first q-point and dipoles
-os.system('cd %s; %s -F yambo_q1.in -J 1'%(folder,yambo))
-#copy dipoles to save
-os.system('cp %s/1/ndb.dip* %s/SAVE'%(folder,folder))
-print('running separate yambo files')
-os.system('parallel :::: jobs.sh')
+    yambo_run = scheduler()
+    yambo_run.add_command('cd %s'%folder)
+    #gather all files
+    for nq in range(1,int(nqpoints)+1): 
+        yambo_run.add_command('cp {0}/ndb.pp_fragment* yambo'.format(str(nq))) 
+        yambo_run.add_command('mv l-{0}* r-{1}* {2}'.format(str(nq),str(nq),str(nq)))
+    print('running gw')
+    yambo_run.add_command('%s -F yambo_run.in -J yambo'%yambo)
+    yambo_run.run()
 
-#gather all the files
-os.system('mkdir %s/yambo'%folder)
-os.system('cd %s; cp 1/ndb.pp* yambo'%folder) 
-os.system('cd %s; cp */ndb.pp_fragment_* yambo'%folder) 
+if args.plot:
 
-y = YamboIn('yambo -p p -g n -V all',folder=folder)
-QPKrange,_ = y['QPkrange']
-y['QPkrange'] = [QPKrange[:2]+[3,6],'']
-y['FFTGvecs'] = [15,'Ry']
-y['NGsBlkXp'] = [1,'Ry']
-y['BndsRnXp'] = [[1,30],'']
-y.write('%s/yambo_run.in'%folder)
-os.system('cd %s; %s -F yambo_run.in -J yambo'%(folder,yambo))
+    #plot the results using yambo analyser
+    ya = YamboAnalyser(folder)
+    print(ya)
+    print('plot all qpoints')
+    ya.plot_gw()
+    print('plot along a path')
+    path = [[   0,   0,   0],
+            [ 0.5,   0,   0],
+            [1./3, 1/3,   0],
+            [0,   0,   0]]
+    ya.plot_gw_path('qp',path)
 
-y.write('%s/yambo_run.in'%folder)
-
-print('running yambo')
-os.system('cd %s; %s -F yambo_run.in -J yambo'%(folder,yambo))
-
-#pack the files in .json files
-pack_files_in_folder(folder)
-
-#plot the results using yambm analyser
-ya = YamboAnalyser()
-print(ya)
-print('plot all qpoints')
-ya.plot_gw('qp')
-print('plot along a path')
-path = [[   0,   0,   0],
-        [ 0.5,   0,   0],
-        [1./3, 1/3,   0],
-        [   0,   0,   0]]
-ya.plot_gw_path('qp',path)
-
-print('done!')
+    print('done!')
