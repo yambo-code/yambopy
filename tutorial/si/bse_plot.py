32b5e6bebb14c300632b56e59aef69822a5998ed
alexmoratalla <alejandro.molina@uv.es>
2019-12-20 11:58:39 +0000

    adding plot of exciton weigths in bands in silicon

--- tutorial/si/bse_plot.py
+++
@@ -1,5 +0,0 @@
-from yambopy import *
-
-ya = YamboAnalyser('bse_calc')
-
-ya.plot_bse('eps_q1',cols=(2,))
--- tutorial/si/clean.sh
+++ tutorial/si/clean.sh
@@ -1,1 +1,1 @@
-rm -rf relax scf nscf bands database gw gw_conv bse bse_conv ip gw_bse relax.log scf.log nscf.log yambo_bse.log yambo_gw.log yambo.log p2y.log rt nscf-dg elphon work elphon.json bands phonons proj.in rt-dg run.sh nscf_flow bse_flow relax_flow bands_flow
+rm -rf relax scf nscf bands database gw gw_conv bse bse_conv ip gw_bse relax.log scf.log nscf.log yambo_bse.log yambo_gw.log yambo.log p2y.log rt nscf-dg elphon work elphon.json bands phonons proj.in rt-dg run.sh nscf_flow bse_flow relax_flow bands_flow __pycache__ gw_calc analyse_gw_conv bse_calc gw_split
