f36aa9f7cf0bf1973d1f498881827b85a561e0f1
alexmoratalla <alejandro.molina@uv.es>
2019-12-22 19:44:24 +0100

    new path of silicon band structure

--- tutorial/si/plot-excitondb.py
+++ tutorial/si/plot-excitondb.py
@@ -10,21 +10,23 @@ ax  = fig.add_axes( [ 0.15, 0.15, 0.80, 0.80 ])
 
 # Define path in reduced coordinates using Class Path
 
-path = Path([ [[0.0,0.0,0.0],'$\Gamma$'],
-           [[0.0,0.5,0.5],'$X$'],
-           [[0.0,0.0,0.0],'$\Gamma$'],
-           [[0.5,0.0,0.0],'$L$']], [20,20,20])
+nk = 50
+path = Path([ [[0.5,0.5,0.5],'$L$'         ],
+              [[0.0,0.0,0.0],'$\Gamma$'    ],
+              [[0.0,0.5,0.0],'$X$'         ],
+              [[3./8.,3./4.,3./8],'$K$' ],
+              [[0.0,0.0,0.0],'$\Gamma$'    ]], [nk,int(nk*2./sqrt(3.0)),int(nk/sqrt(6.)),int(nk*3./sqrt(3.0)/2.)] )
 
 # Load databases
 
 # SAVE database
-save = YamboSaveDB.from_db_file(folder='bse_flow/t0/SAVE')
+save = YamboSaveDB.from_db_file(folder='bse_calc/SAVE')
 
 # Lattice information
-lat  = YamboLatticeDB.from_db_file(filename='bse_flow/t0/SAVE/ns.db1')
+lat  = YamboLatticeDB.from_db_file(filename='bse_calc/SAVE/ns.db1')
 
 # Exciton database read from db file
-yexc = YamboExcitonDB.from_db_file(lat,filename='ndb.BS_diago_Q01',folder='bse_flow/t0/run')
+yexc = YamboExcitonDB.from_db_file(lat,filename='ndb.BS_diago_Q01',folder='bse_calc/yambo')
 
 print("Ground state energy: %lf" % yexc.eigenvalues[0].real )
 print("Intensity: %lf" % (yexc.get_intensities()[0]+yexc.get_intensities()[1]) )
@@ -32,7 +34,7 @@ print("1st-excited state energy: %lf" % yexc.eigenvalues[2].real )
 print("Intensity: %lf" % (yexc.get_intensities()[2]+yexc.get_intensities()[3]) )
 
 # List of states to be merged
-states = [3,4]
+states = [1,2]
 
 # 1. Plot exciton weights in band structure NOT interpolated
 
