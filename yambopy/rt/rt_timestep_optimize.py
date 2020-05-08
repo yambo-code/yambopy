e3e3b97d6fe9e18b08671b772633bf05e681672b
Fulvio Paleari <fulvio.paleari90@gmail.com>
2020-05-08 17:37:10 +0200

    Addition: now YamboRTOptimize computes the dipoles only once and separately at the beginning of the convergence run.

--- yambopy/rt/rt_timestep_optimize.py
+++ yambopy/rt/rt_timestep_optimize.py
@@ -28,7 +28,8 @@ class YamboRTStep_Optimize():
         self.yambo_rt = yambo_rt
 
         self.create_folder_structure(SAVE_path)
-
+        
+        self.COMPUTE_dipoles()
         conv = self.FIND_values()
         self.RUN_convergence(conv)
 
@@ -54,17 +55,32 @@ class YamboRTStep_Optimize():
         conv = { 'RTstep': [[1,5,10,11,12,15],'as'] } #Hardcoded. TO DO: dynamical.
         return conv
 
-    def COMPUTE_dipoles(self):
+    def COMPUTE_dipoles(self,DIP_folder='dipoles'):
         """
         Compute the dipoles once and for all
         """
-        #Create input; take dipoles variables (i.e. bands and parall) from self.yin; run in folder
+        ydipoles = YamboIn()
+        ydipoles.arguments.append('dipoles')
+        #ydipoles.arguments.append('negf')
+        ydipoles['DIP_ROLEs'] = self.yin['DIP_ROLEs']
+        ydipoles['DIP_CPU'] = self.yin['DIP_CPU']
+        ydipoles['HARRLvcs'] = self.yin['HARRLvcs']
+        ydipoles['DipBands'] = self.yin['DipBands']
+        ydipoles.write('%s/dipoles.in'%self.RUN_path)
+        print("Running dipoles...")
+        shell = self.scheduler()
+        shell.add_command('cd %s'%self.RUN_path)
+        #THIS must be replaced by a more advanced submission method
+        shell.add_command('%s -F dipoles.in -J %s -C %s 2> %s.log'%(self.yambo_rt,DIP_folder,DIP_folder,DIP_folder))
+        shell.run()
+        shell.clean() 
+        self.DIP_folder = DIP_folder
 
     def RUN_convergence(self,conv):
         """
         Run the yambo_rt calculations
         """
-
+        print("Running RT time step convergence...")
         def run(filename):
             """ Function to be called by the optimize function """
             folder = filename.split('.')[0]
@@ -73,9 +89,8 @@ class YamboRTStep_Optimize():
             shell = self.scheduler()
             shell.add_command('cd %s'%self.RUN_path)
             #THIS must be replaced by a more advanced submission method
-            shell.add_command('%s -F %s -J %s -C %s 2> %s.log'%(self.yambo_rt,filename,folder,folder,folder))
+            shell.add_command('%s -F %s -J %s,%s -C %s 2> %s.log'%(self.yambo_rt,filename,folder,self.DIP_folder,folder,folder))
             shell.run()
             shell.clean()
 
-        #ADD an initial calculation of the DIPOLES in order not to recompute them (use -J "X/Y" within the runs
         self.yin.optimize(conv,folder=self.RUN_path,run=run,ref_run=False)
