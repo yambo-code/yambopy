3623556ed695a5902bda8b85d781926073c0d56c
Fulvio Paleari <fulvio.paleari90@gmail.com>
2020-09-03 15:04:18 +0200

    Started working on schedulers

--- yambopy/rt/rt_timestep_optimize.py
+++ yambopy/rt/rt_timestep_optimize.py
@@ -12,15 +12,22 @@ class YamboRTStep_Optimize():
     - Needs an initialised RT SAVE
     - Needs an RT input 
     - Optional arguments: directory paths, max time step, time step increase, max number of runs
+    - Optional: add convergence loop with respect to field intensity
 
     Example of use:
         .. code-block:: python
     
             YamboRTStep_Optimize(input_path,SAVE_path,RUN_path,ref_time,TStep_MAX,TStep_increase,NSimulations)
+    
+        .. code-block:: python
+    
+            YamboRTStep_Optimize(input_path,SAVE_path,RUN_path,ref_time,TStep_MAX,TStep_increase,NSimulations,FieldInts)
 
     """
 
-    def __init__(self,input_path='./yambo.in',SAVE_path='./SAVE',RUN_path='./RT_time-step_optimize',yambo_rt='yambo_rt',ref_time=60,TStep_MAX=30,TStep_increase=5,NSimulations=6,db_manager=True,tol_eh=1e-4,tol_pol=5e-3,Tpoints_min=30,plot_results=True):
+    def __init__(self,input_path='./yambo.in',SAVE_path='./SAVE',RUN_path='./RT_time-step_optimize',yambo_rt='yambo_rt',\
+                 ref_time=60,TStep_MAX=30,TStep_increase=5,NSimulations=6,FieldInts=None,\
+                 tol_eh=1e-4,tol_pol=5e-3,Tpoints_min=30,plot_results=True):
         #Setting global variables
         self.scheduler = Scheduler.factory
         input_path, input_name = input_path.rsplit('/',1)
