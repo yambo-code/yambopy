ffc008fe0015af45d19e78f5ee0452bbab9d0535
Fulvio Paleari <fulvio.paleari90@gmail.com>
2020-10-05 14:25:13 +0200

    Removed redundant code between save and gkkp scripts

--- scripts/gkkp.py
+++ scripts/gkkp.py
@@ -1,5 +1,6 @@
 import os
 from yambopy import *
+from scripts import generate_save
 from schedulerpy import *
 import argparse
 
@@ -8,7 +9,7 @@ Script to produce a yambo SAVE folder along with *unexpanded* gkkp matrix elemen
 Usable to quickly initialize a yambo_ph (equilibrium) calculation.
 
 Inputs:
- 1. --nscf_dir='path/to/nscf/save/folder'
+ 1. --nscf_dir='path/to/nscf/save/folder' [OPTIONAL]
  2. --elph_dir='path/to/dfpt/elph_dir/folder'
  3. --yambo='path/to/yambo/executables' [OPTIONAL]
 
@@ -17,47 +18,23 @@ NB:
 
 """
 
-def generate_SAVE(database,qe_save,y_dir,scheduler,out):
-    """
-    Generate SAVE folder from QE nscf calculation
-    """
-    #check if the nscf cycle is present
-    if os.path.isdir(qe_save):
-        out.msg('nscf calculation found!')
-    else:
-        out.msg('nscf calculation not found!')
-        exit()
-
-    p2y = "%s/p2y"%y_dir
-    yambo = "%s/yambo"%y_dir
-
-    #check if the SAVE folder is present
-    if os.path.isdir('%s/SAVE'%database):
-        out.msg('SAVE database found!')
-    if not os.path.isdir('%s/SAVE'%database):
-        out.msg('preparing yambo database')
-
-        p2y_run = scheduler()
-        p2y_run.add_command('mkdir -p %s'%database)
-        p2y_run.add_command('cd %s; %s > p2y.log ; cd -'%(qe_save,p2y))
-        p2y_run.add_command('cd %s; %s > yambo.log ; cd -'%(qe_save,yambo))
-        p2y_run.add_command('cd %s; mv SAVE %s ; cd -'%(qe_save,database))
-        p2y_run.run()
-
-def generate_gkkp(database,elph_save,y_dir,scheduler):
+def generate_gkkp(database,qe_save,elph_save,y_dir,scheduler):
     """
     Read gkkp from dfpt calculation
     """
+    # Generate SAVE folder if nscf_path is given
+    if qe_save != "": generate_save.generate_save(database,qe_save,y_dir,scheduler)
+    
     #check if gkkp databases are already present
     if os.path.isfile('%s/SAVE/ndb.elph_gkkp'%database) or os.path.isfile('%s/SAVE/ndb.elph_gkkp_expanded'%database):
-        out.msg('gkkp found!')
+        print('gkkp found!')
     else:
         #check if the elph_dir folder is present
         if not os.path.isfile('%s/s.dbph_000001'):
-            out.msg('problem with dbph databases at %s'%elph_dir)
+            print('problem with dbph databases at %s'%elph_dir)
             exit()
         else:
-            out.msg('reading gkkp')
+            print('reading gkkp')
 
             yambo_ph = "%s/yambo_ph"%y_dir
             ypp_ph = "%s/ypp_ph"%y_dir
@@ -81,11 +58,11 @@ def generate_gkkp(database,elph_save,y_dir,scheduler):
             yppph_run.add_command('cd %s ; %s -F %s; cd -'%(database,ypp_ph,filnm2))
             yppph_run.run()
             if not os.path.isfile('%s/SAVE/ndb.elph_gkkp'%database):
-                out.msg('[ERROR] ndb.elph_gkkp databases not created. Check the logs.')
+                print('[ERROR] ndb.elph_gkkp databases not created. Check the logs.')
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate SAVE folder including gkkp databases')
-    parser.add_argument('-nscf','--nscf_dir', type=str,help='<Required> Path to nscf save folder', required=True)
+    parser.add_argument('-nscf','--nscf_dir', type=str, default="", help='<Optional> Path to nscf save folder')
     parser.add_argument('-elph','--elph_dir', type=str,help='<Required> Path to elph_dir folder',required=True)
     parser.add_argument('-y','--yambo_dir', type=str, default="", help='<Optional> Path to yambo executables')
     args = parser.parse_args()
@@ -97,12 +74,4 @@ if __name__ == "__main__":
     database = './'
     scheduler = Scheduler.factory
 
-    # Start IO
-    yf = YamboIO(out_name='YAMBOPY_GKKPsetup.log',out_path=database,print_to_shell=True)
-    yf.IO_start()
-
-    initialize_SAVE(database,nscf_dir,yambo_dir,scheduler,yf)
-    initialize_gkkp(database,elph_dir,yambo_dir,scheduler,yf)
-
-    # End IO
-    yf.IO_close()
+    generate_gkkp(database,nscf_dir,elph_dir,yambo_dir,scheduler)
