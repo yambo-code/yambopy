bc17384c367383a94b037b36dacfd80e7ab04fef
Fulvio Paleari <fulvio.paleari90@gmail.com>
2020-12-03 20:02:18 +0100

    Progress in band generation script

--- scripts/generate_bands.py
+++ scripts/generate_bands.py
@@ -1,7 +1,10 @@
 import yaml
 import argparse
 import numpy as np
+from math import sqrt
 import xml.etree.ElementTree as ET
+from qepy import xml
+from qepy import bravais
 
 """
 Script to produce band structure data and visualization from QE.
@@ -32,18 +35,6 @@ The input parameters are:
 
  - n_valence_bands: AUTOMATICALLY DETERMINE
 """
-def get_xml_attrib(xml_root,tag,attrib,repeated=False):
-    """ Extract xml attribute
-    """
-    attribute =  None
-    for element in xml_root.iter():
-        if element.tag == tag: 
-            attribute = element.attrib[attrib]
-            if repeated: break
-
-    if attribute is None: raise ValueError('xml attribute %s not found'%attrib)
-    else: return attribute
-
 def read_input(inp_file):
     """
     Read input file in yaml format (see above docstring)
@@ -74,195 +65,142 @@ def read_input(inp_file):
         shift_Delta_c_v = None
     
     return save_dir, prefix, KPTs, KPTs_labels, shift_Delta_c_v
-
-def setup_BZ_points(input_params):
+    
+def get_data_from_xml(input_params):
     """
-    Perform preliminary operations such as transform in cartesian
-    coordinates and find kpath lengths.
+    Read the data-file-schema.xml to obtain all the needed variables
+    
+    TO BE WRAPPED WITH A DEDICATED PWXML CLASS
     """
     save_dir, prefix, KPTs, KPTs_labels, shift_Delta_c_v = input_params
     
     # Input files from QE
-    eig_xml   = 'eigenval.xml' 
     datafile  = 'data-file-schema.xml'
-    datafile_xml = ET.parse( "%s/%s"%(save_dir, datafile)).getroot()
+    datafile_xml = ET.parse( "%s/%s"%(save_dir, datafile))
+    
+    # Get lattice vectors
+    lat_vecs = []
+    lat_vecs_tags = ['a1','a2','a3']
+    for tag in lat_vecs_tags:
+        lat_vecs.append( xml.get_xml_data(datafile_xml,tag,as_type=float,repeated=False) )
+    lat_vecs = np.array(lat_vecs)
+    
+    # Get kpoint number
+    nkpoints = xml.get_xml_data(datafile_xml,'nk',as_type=int,repeated=True)
+    
+    # Get band number
+    nbands   = xml.get_xml_data(datafile_xml,'nbnd',as_type=int,repeated=True)
+    
+    # Get kpoints cartesian coordinates
+    kpts_cart, eigen, occs,= xml.get_xml_nk_bands(datafile_xml)
+    
+    # Get Fermi energy
+    fermi_e = xml.get_xml_data(datafile_xml,'fermi_energy',as_type=float)
+    
+    # Scissor operator check
+    if shift_Delta_c_v is not None:
+        occ_kind = xml.get_xml_data(datafile_xml,'occupations_kind',as_type=str)
+        if occ_kind!='fixed': 
+            print('[WARNING]: if your system is a metal, the scissor shift will NOT work!')
+    
+    # Get topmost valence band (only valid for gapped systems)
+    n_VBM = np.sum(occs[0])
+    
+    # Get eigenvalues
+    eigen, s_eigen = process_bands(eigen,kpts_cart,fermi_e,shift_Delta_c_v,n_VBM)
+    
+    return lat_vecs, nkpoints, nbands, kpts_cart, eigen, s_eigen
+    
+def setup_BZ_points(input_params,output_data):
+    """
+    Perform preliminary operations such as transform in cartesian
+    coordinates and find kpath lengths.
+    
+    ***ASSUMING SAME NUMBER OF POINTS FOR EACH SEGMENT***)
+    """
+    save_dir, prefix, KPTs, KPTs_labels, shift_Delta_c_v  = input_params
+    lat_vecs, nkpoints, nbands, kpts_cart, eigen, s_eigen = output_data
 
-    # Assign path features
+    # Describe BZ symmetry lines
     nKPTs = len(KPTs)
-    print("=== PATH ===")
-    print("directions: %d"%(nKPTs-1))
-    if KPTs[-1]==KPTs[0]: print("(Closed path)")
-    else: print("(Open path)")
+    nkpt_per_direction = (nkpoints-1)/(nKPTs-1)
 
-    # Get lattice type 
-    ibrav = get_xml_attrib(datafile_xml,'atomic_structure','bravais_index',repeated=True)
-    ibrav = int(ibrav)
-    lat_type = lattice_dictionary(ibrav)
-    exit()
-    ## CONTINUE FROM HERE!
-    if datafile_xml.find("CELL/CELL_DIMENSIONS") is None: 
-        raise ValueError('CELL_PARAMETERS not found')
-    else: 
-        cell_param = np.array( datafile_xml.find("CELL/CELL_DIMENSIONS").text.split() ).astype(float)
-
-    # Transform edge kpoints from crystal to cartesian coordinates
-    G_r = crys_to_car(KPTs,lat_type,cell_param)
+    # Metric tensor in reciprocal space
+    G_r = bravais.crys_to_car(lat_vecs,reciprocal_space=True)
 
     # Length of path lines
     KPTs = np.array(KPTs)
     KPTdist = np.array([ KPTs[i+1]-KPTs[i] for i in range(nKPTs-1) ])
 
-    # ||x||^2 = x^T G_r x
+    # Transform edge kpoints from crystal to cartesian coordinates:
+    #               ||x||^2 = x^T G_r x
     KPT_lengths = np.array([ sqrt(np.dot(vec, np.dot(G_r, vec))) for vec in KPTdist ])
     KPT_lengths = KPT_lengths/np.max(KPT_lengths)
+    
+    # Generating scaled steps
+    kstps = np.zeros(nkpoints)
+    points = []
+    for i in range(1,nKPTs):
+        n_l, n_r = np.array([i-1, i])*nkpt_per_direction
+        n_l = int(n_l)
+        n_r = int(n_r)
+        for ik in range(n_l+1,n_r+1): kstps[ik] = kstps[ik-1]+KPT_lengths[i-1]
+        points.append(kstps[n_r])
+
+    # Print info
+    setup_info(nKPTs,nkpt_per_direction,KPTs,KPT_lengths,nkpoints,nbands,points)
+    
+    if shift_Delta_c_v is not None: return prefix,nkpoints,nbands,kstps,eigen,s_eigen    
+    else: return prefix,nkpoints,nbands,kstps,eigen
+        
+    
+def setup_info(nKPTs,nkpt_per_direction,KPTs,KPT_lengths,nkpoints,nbands,points):
+    """
+    Print information about the system
+    """
+    # BZ info
+    print("=== PATH ===")
+    print("directions: %d"%(nKPTs-1))
+    if np.array_equal(KPTs[-1],KPTs[0]): print("(Closed path)")
+    else: print("(Open path)")  
     print("ratios: ")
-    print(KPT_lengths)
-
-def crys_to_car(KPTs,lattice,cell):
-    """ Calculation of the metric tensors
-    """
+    print(KPT_lengths) 
     
-    lat_vecs = lattice_type(lattice,cell)
-    #Metric tensor
-    G = np.array([ [np.dot(a1,a2) for a1 in lat_vecs] for a2 in lat_vecs ])
-    #Metric tensor in reciprocal space
-    G_r = np.linalg.inv(G)
-    return G_r
-
-def lattice_dictionary(ibrav):
-    """
-    Dictionary with text descriptions for ibrav numbers
-    """    
-    bravais_lattices = {
-    0 : 'free',
-    1 : 'cubic P (sc)',
-    2 : 'cubic F (fcc)',
-    3 : 'cubic I (bcc)',
-    5 : 'Trigonal R, 3fold axis c',
-    6 : 'Tetragonal P (st)',
-    7 : 'Tet ragonal I (bct)',
-    8 : 'Orthorhombic P',
-    9 : 'Orthorhombic base-centered(bco)',
-    10 : 'Orthorhombic face-centered',
-    11 : 'Orthorhombic body-centered',
-    12 : 'Monoclinic P, unique axis c',
-    13 : 'Monoclinic base-centered',
-    14 : 'Triclinic'
-    }
-    try: bravais_lattices[ibrav]
-    except KeyError: print("This lattice type is not implemented ):")
-    else: return bravais_lattices[ibrav]
+    # Bands info 
+    print("=== BAND PLOT ===")
+    print("nkpoints: %d"%nkpoints)
+    print("kpoint density per direction: %d"%nkpt_per_direction)
+    print("nbands: %d"%nbands)  
     
-def lattice_type(lattice,cell):
+    # Symmetry points info
+    print("Internal high-symmetry points at: ")
+    print(points)  
+    
+def process_bands(eigen,kpts_cart,fermi_e,shift_Delta_c_v,n_val):
     """
-    This function contains the Bravais lattice basis vector coordinates (ibrav in QE)
+    Fix Fermi level to zero, apply scissor shift, return useful dictionary for plotting 
+    
+    Output:
+        eigenvalues and shifted eigenvalues (equal if not scissor applied)
     """
-    if lattice=='Hexagonal and Trigonal P':
-        a    = cell[0]
-        c_a  = cell[2] #c/a
-        vecs = a*np.array([ [  1.,          0.,  0. ], 
-                            [-0.5, sqrt(3.)/2.,  0. ],
-                            [  0.,          0., c_a ] ])
-    if lattice=='free':
-        vecs = np.array([ datafile_xml.find("CELL/DIRECT_LATTICE_VECTORS/a1").text.split(),
-                          datafile_xml.find("CELL/DIRECT_LATTICE_VECTORS/a2").text.split(),
-                          datafile_xml.find("CELL/DIRECT_LATTICE_VECTORS/a3").text.split() ]).astype(float)
-    if lattice=='cubic P (sc)':
-        a    = cell[0]
-        vecs = a*np.array([ [ 1., 0., 0. ],
-                            [ 0., 1., 0. ],
-                            [ 0., 0., 1. ] ])
-    if lattice== 'cubic F (fcc)':
-        a    = cell[0]
-        vecs = a/2.*np.array([ [-1., 0., 1. ],
-                               [ 0., 1., 1. ],
-                               [-1., 1., 0. ] ]) 
-    if lattice=='cubic I (bcc)':
-        a    = cell[0]
-        vecs = a/2.*np.array([ [ 1., 1., 1. ],
-                               [-1., 1., 1. ],
-                               [-1.,-1., 0. ] ])
-    if lattice=='Trigonal R, 3fold axis c':
-        a    = cell[0]
-        c_g  = cell[3] #cos(gamma)
-        tx, ty, tz = [ sqrt((1.-c_g)/2.), sqrt((1.-c_g)/6.), sqrt((1.+2*c_g)/3.) ]
-        vecs = a*np.array([ [ tx,  -ty, tz ],
-                            [ 0., 2*ty, tz ],
-                            [-tx,  -ty, tz ] ]) 
-    if lattice=='Tetragonal P (st)':
-        a    = cell[0]
-        c_a  = cell[2]
-        vecs = a*np.array([ [ 1., 0.,  0. ],
-                            [ 0., 1.,  0. ],
-                            [ 0., 0., c_a ] ])
-    if lattice=='Tetragonal I (bct)':
-        a    = cell[0]
-        c_a  = cell[2]
-        vecs = a/2.*np.array([ [ 1.,-1., c_a ],
-                               [ 1., 1., c_a ],
-                               [-1.,-1., c_a ] ])
-    if lattice=='Orthorhombic P':
-        a    = cell[0]
-        b_a  = cell[1] #b/a
-        c_a  = cell[2]
-        vecs = a*np.array([ [ 1.,  0.,  0. ],
-                            [ 0., b_a,  0. ],
-                            [ 0.,  0., c_a ] ])
-    if lattice=='Orthorhombic base-centered(bco)':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        vecs = a*np.array([ [ 1./2., b_a/2.,  0. ],
-                            [-1./2., b_a/2.,  0. ],
-                            [    0.,     0., c_a ] ])
-    if lattice=='Orthorhombic face-centered':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        vecs = a/2.*np.array([ [ 1.,  0., c_a ],
-                               [ 1., b_a,  0. ],
-                               [ 0., b_a, c_a ] ])
-    if lattice=='Orthorhombic body-centered':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        vecs = a/2.*np.array([ [ 1., b_a, c_a ],
-                               [-1., b_a, c_a ],
-                               [-1.,-b_a, c_a ] ])
-    if lattice=='Monoclinic P, unique axis c':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        c_g  = cell[3]
-        s_g  =+sqrt(1.-c_g*c_g)
-        vecs = a*np.array([ [      1.,      0.,  0. ],
-                            [ b_a*c_g, b_a*s_g,  0. ],
-                            [      0.,      0., c_a ] ])
-    if lattice=='Monoclinic base-centered':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        c_g  = cell[3]
-        s_g  =+sqrt(1.-c_g*c_g)
-        vecs = a*np.array([ [   1./2.,      0.,-c_a/2. ],
-                            [ b_a*c_g, b_a*s_g,     0. ],
-                            [   1./2.,      0., c_a/2. ] ])
-    if lattice=='Triclinic':
-        a    = cell[0]
-        b_a  = cell[1]
-        c_a  = cell[2]
-        c_a  = cell[3] #cos(alpha)
-        c_b  = cell[4] #cos(beta) 
-        c_g  = cell[5] #cos(gamma)
-        s_g  =+sqrt(1.-c_g*c_g)
-        V2   = a**6.*b_a**2.*c_a**2.*(1.+2.*c_a*c_b*c_g-c_a*c_a-c_b*c_b-c_g*c_g)
-        vecs = a*np.array([ [      1.,      0., 0. ],
-                            [ b_a*c_g, b_a*s_g, 0. ],
-                            [ c_a*c_b, c_a*(c_a-c_b*c_g)/s_g, c_a*sqrt(V2)/(a*b_a*a*c_a*a)/s_g ] ])  
-    #
-    try: vecs
-    except NameError: print("This lattice type is not implemented ):")
-    else: return vecs
+    # Rescale in eV and shift Fermi level to zero
+    ha2ev  = 27.211396132
+    processed_eigen  = ha2ev*(eigen-fermi_e)
+    
+    # Apply scissor shift
+    shifted_eigen = processed_eigen
+    if shift_Delta_c_v is not None:
+        scissor = shift_Delta_c_v
+        top_v, bottom_c = eigen[:,n_val-1], eigen[:,n_val]
+        ind_k_dir_gap = np.argmin(bottom_c-top_v)
+        ev_max, ec_min = top_v[ind_k_dir_gap], bottom_c[ind_k_dir_gap]
+        shifted_eigen = processed_eigen
+        for ik in range( len(kpts_cart) ):
+            for ib in range( len(processed_eigen[0]) ):
+                if ib<n_val: shifted_eigen[ik][ib] = ev_max-(ev_max-eigen[ik][ib])*scissor[2]
+                else:        shifted_eigen[ik][ib] = ec_min+scissor[0]+(eigen[ik][ib]-ec_min)*scissor[1]
+
+    return processed_eigen, shifted_eigen
 
 if __name__ == "__main__":
 
@@ -272,12 +210,14 @@ if __name__ == "__main__":
 
     inp = args.band_input 
     
-    # Read input file
+    # Read user input file
     input_params = read_input(inp)
     
-    # BZ setup 
-    setup_BZ_points(input_params)
+    # Read data from .xml file
+    output_data = get_data_from_xml(input_params)
     
+    # BZ setup 
+    data_to_plot = setup_BZ_points(input_params,output_data)
     
     
 
