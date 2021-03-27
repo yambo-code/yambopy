08ac21b0f41da488c21442915eaa6f257df6ed2c
Miki Bonacci <miki.bonacci@unimore.it>
2021-03-27 12:55:16 +0100

    sometimes o-qp gives error in YamboFile

--- yamboparser/yambofolder.py
+++ yamboparser/yambofolder.py
@@ -23,9 +23,12 @@ class YamboFolder():
         for dirname, dirnames, filenames in os.walk(path):
             # iterate over all the files in the folder
             for filename in filenames:
-                y = YamboFile(filename, folder=dirname)
-                if y.type !='unknown': #checks if the file is of a known type
-                    self.yambofiles.append(y)
+                try:
+                    y = YamboFile(filename, folder=dirname)
+                    if y.type !='unknown': #checks if the file is of a known type
+                        self.yambofiles.append(y)
+                except:
+                    pass
 
     def get_data(self):
         """
