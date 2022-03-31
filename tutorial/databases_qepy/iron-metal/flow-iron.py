78b3fe09ec1e4eee4fe2b539a6a487e90880b63b
alexmoratalla <amimanera@gmail.com>
2022-03-31 16:03:41 +0200

    flow works directly. duplicate files removed

--- tutorial/databases_qepy/iron-metal/flow-iron.py
+++ tutorial/databases_qepy/iron-metal/flow-iron.py
@@ -43,57 +43,15 @@ ce   = dict(cell_dynamics='sd')
 
 material = dict(lattice=lattice,atypes=atypes,atoms=atoms,occupations=occ,electrons=elec,ions=ion,cell=ce)
 
-# Relax Class
-qe_atoms_task, qe_cell_task, qe_scf_task = PwRelaxTasks(material,kpoints,ecut,scf_conv_thr=1.e-8,cell_dofree='all',starting_magnetization=[2],spin="polarized",pseudo_dir=pseudo_dir)
-
 # Bands Class
 qe_bands_scf, qe_bands_bands = PwBandsTasks(material,kpoints,ecut,path_bands,path_kpoints,scf_conv_thr=1.e-8,starting_magnetization=[2],spin="polarized",pseudo_dir=pseudo_dir)
 qe_bands_scf.pwinput.set_kpoints(kpoints,shiftk)
 
-# Nscf Class
-qe_nscf_scf, qe_nscf_nscf, p2y_task = PwNscfTasks(material,kpoints,ecut,nscf_bands,nscf_kpoints,scf_conv_thr=1.e-8,starting_magnetization=[2],spin="polarized",pseudo_dir=pseudo_dir)
+# Scheduler
+qe_bands_scf.scheduler_setup(sch)
+qe_bands_bands.scheduler_setup(sch)
 
-def relax():
-    # Scheduler
-    qe_atoms_task.scheduler_setup(sch)
-    qe_cell_task.scheduler_setup(sch)
-    qe_scf_task.scheduler_setup(sch)
-
-    relax_flow = YambopyFlow.from_tasks('relax',[qe_atoms_task,qe_cell_task,qe_scf_task])
-    relax_flow.create(agressive=True)
-    relax_flow.run()
-
-def bands():
-
-    # Scheduler
-    qe_bands_scf.scheduler_setup(sch)
-    qe_bands_bands.scheduler_setup(sch)
-
-    bands_flow = YambopyFlow.from_tasks('bands',[qe_bands_scf,qe_bands_bands])
-    bands_flow.create(agressive=True)
-    bands_flow.run()
-
-def nscf():
-
-    # Scheduler
-    qe_nscf_scf.scheduler_setup(sch)
-    qe_nscf_nscf.scheduler_setup(sch)
-    p2y_task.scheduler_setup(sch)
-
-    nscf_flow = YambopyFlow.from_tasks('nscf3',[qe_nscf_scf,qe_nscf_nscf,p2y_task])
-    nscf_flow.create(agressive=True)
-    nscf_flow.run()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Choose Yambopy Task.')
-    parser.add_argument('-r' ,'--relax',       action="store_true", help='Structural relaxation Task')
-    parser.add_argument('-b' ,'--bands',       action="store_true", help='Scf and Bands calculation Task')
-    parser.add_argument('-n' ,'--nscf',        action="store_true", help='Scf, Nscf and p2y calculation Task')
-    args = parser.parse_args()
-
-if args.relax:
-   relax()
-if args.bands:
-   bands()
-if args.nscf:
-   nscf()
+# Run Flow
+bands_flow = YambopyFlow.from_tasks('bands',[qe_bands_scf,qe_bands_bands])
+bands_flow.create(agressive=True)
+bands_flow.run()
