3b671b0ef095715f21f4654f266d5423306c7ed6
Fulvio Paleari <fulvio.paleari90@gmail.com>
2022-06-09 19:03:14 +0200

    Fixed missing time reversal in symmetry expansion of pwxml

--- qepy/pwxml.py
+++ qepy/pwxml.py
@@ -523,28 +523,41 @@ class PwXML():
         #nsym
         nrot = int(self.datafile_xml.findall("output/symmetries/nrot")[0].text.strip())        
         self.nsym = int(self.datafile_xml.findall("output/symmetries/nsym")[0].text.strip())        
+        no_t_rev = ( self.datafile_xml.findall("input/symmetry_flags/no_t_rev")[0].text.strip() == "true" )
+        self.nsym_with_trev = 2*self.nsym 
 
         # data
-        sym_red = np.zeros((self.nsym,3,3))
-        symmetries = self.datafile_xml.findall("output/symmetries/symmetry/rotation")
-        symmetries = symmetries[:self.nsym]
+        if not no_t_rev: sym_red = np.zeros((self.nsym_with_trev,3,3))
+        else:            sym_red = np.zeros((self.nsym,3,3))
+        symmetries = self.datafile_xml.findall("output/symmetries/symmetry/rotation") # All rotations
+        symmetries = symmetries[:self.nsym] # Retain only point group symmetries with no trev
         #sym_info   = self.datafile_xml.findall("output/symmetries/symmetry/info")
         #NB: sym_info[:].attrib['class'] contains irrep names if found
 
-        #read (non-symmorphic) symmetris
+        #read (non-symmorphic) symmetries
         for i in range(self.nsym):
             sym = np.array( [float(x) for x in symmetries[i].text.strip().split()] ).reshape(3,3)
             sym_red[i] = sym.T # symmetries are saved as the transposed in the .xml 
         self.sym_red = sym_red.astype(int)
 
+        #convert to c.c.
         self.sym_car = self.sym_red_car()
+
+        #check for time reversal and apply it to sym_car
+        if not no_t_rev: self.apply_t_rev()
+
+    def apply_t_rev(self):
+        """
+        Add T*S rotation matrices
+        """
+        for n in range(self.nsym,self.nsym_with_trev): self.sym_car[n]=-1*self.sym_car[n-self.nsym]
 
     def sym_red_car(self):
         """
         Transform symmetry ops. in Cartesian coordinates
         """
         lat = np.array(self.cell)
-        sym_car = np.zeros([self.nsym,3,3],dtype=float)
+        sym_car = np.zeros([len(self.sym_red),3,3],dtype=float)
         for n,s in enumerate(self.sym_red):
             sym_car[n] = np.dot( np.linalg.inv(lat), np.dot(s, lat ) ).T
         return sym_car
