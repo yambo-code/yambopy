cdb49ce50cfaefc809cb8d4193ab3b2cb1adbd7f
Fulvio Paleari <fulvio.paleari90@gmail.com>
2023-06-26 15:43:57 +0200

    Updates to em1sdb: compute asymmetrical epsilon, computed q-space (truncated) Coulomb potential. Started rotate_em1s class to expand IBZ->BZ

--- yambopy/__init__.py
+++ yambopy/__init__.py
@@ -26,6 +26,9 @@ Modules:
         - YamboExcitonWeight: read the excitonic weights from the ypp output file
         - YamboBSEAbsorptionSpectra: generate a .json file with the bse absorption calculation (including information about the excitons)
 
+    em1s
+        - YamboEm1sRotate: rotate em1s from IBZ to BZ
+        - YamboEm1sExpand: expand em1s from unit cell to supercell [IN DEVELOPMENT]
     analyse:
         - YamboAnalyser: read .json files generated with yamboout and plot them together
 """
@@ -71,6 +74,9 @@ from yambopy.bse.excitonwf import *
 from yambopy.bse.excitonweight import *
 from yambopy.bse.bse_absorption import *
 from yambopy.bse.bse_dispersion import *
+
+#em1s/static screening operations files
+from yambopy.em1s.em1s_rotate import *
 
 #analyse stuff
 from yambopy.analyse import *
