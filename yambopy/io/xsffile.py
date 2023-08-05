89724052c31274a020009c16ab07fb9904195cf1
Riccardo Reho <riccardoreho95@gmail.com>
2023-08-05 22:01:35 +0200

    Fix bug relatives to read/write datagrid

--- yambopy/io/xsffile.py
+++ yambopy/io/xsffile.py
@@ -42,7 +42,7 @@ class YamboXsf():
     
     def add_grid_data(cls, grid_name, block_dim, sub_grid_name, sub_grid_size, sub_grid_dim, sub_grid_origin,\
         sub_grid_vectors, data_array):
-        cls.add_grid_data.append((grid_name, block_dim, sub_grid_name, sub_grid_size, sub_grid_dim, sub_grid_origin, \
+        cls.grid_data.append((grid_name, block_dim, sub_grid_name, sub_grid_size, sub_grid_dim, sub_grid_origin, \
             sub_grid_vectors, data_array))      
 
     def set_lattice(cls,lattice):
@@ -72,28 +72,31 @@ class YamboXsf():
                     f.write(f"{symbol} {position[0]} {position[1]} {position[2]}\n")
 
         # wride grid_data for each sub_block
-            if np.any(self.grid_data):
+            if (len(self.grid_data)!=0):
                 new_block_counter = 0 
-                for grid_name, block_dim, sub_grid_name, sub_grid_size, sub_grid_origin, sub_grid_vectors, sub_data_array in self.grid_data:
-                    if(new_block_counter == len(sub_grid_name) or new_block_counter==0): f.write("BEGIN_BLOCK_DATAGRID_\n")
-                    f.write(f"  {grid_name}\n")
-                    f.write("  BEGIN_DATAGRID_".join(sub_grid_name[i] for i in range(2,len(sub_grid_name)))+"\n")
-                    f.write(f"    {sub_grid_size}\n")
-                    f.write(f"    {sub_grid_origin}\n")
-                    f.write("    ".join(str(sub_grid_vectors[i]) for i in range(sub_grid_vectors.shape[0]))+"\n")
-                    for k in range(sub_data_array.shape[2]):
+                for grid_name, block_dim, sub_grid_name, sub_grid_size, sub_grid_dim, sub_grid_origin, sub_grid_vectors, sub_data_array in self.grid_data:
+                    if(new_block_counter == len(sub_grid_name) or new_block_counter==0): f.write(f"BEGIN_BLOCK_DATAGRID_{block_dim}D\n")
+                    f.write(f"\t{grid_name}\n")
+                    f.write(f"\tBEGIN_DATAGRID_{sub_grid_dim}D\n")
+                    f.write(f"".join("\t"+str(sub_grid_size[i]) for i in range(len(sub_grid_size)))+"\n")
+                    f.write(f"".join("\t"+str(sub_grid_origin[i]) for i in range(len(sub_grid_origin)))+"\n")
+                    for i in range(0,sub_grid_vectors.shape[0]):
+                        f.write(f"".join("\t"+str(sub_grid_vectors[i][j]) for j in range(sub_grid_vectors.shape[1]))+"\n")
+                    for i in range(sub_data_array.shape[0]):
                         for j in range(sub_data_array.shape[1]):
-                            f.write(f"      ".join(str(sub_data_array[i,j,k] for i in range(sub_data_array.shape[0])))+"\n")
-                    f.write("  END_DATAGRID_".join(sub_grid_name[i] for i  in range(2,len(sub_grid_name)))+"\n")
+                            for k in range(sub_data_array.shape[2]):
+                                #print(sub_data_array[i,j,k]) 
+                                f.write(f"\t{sub_data_array[i,j,k]}\n")
+                    f.write(f"\tEND_DATAGRID_{sub_grid_dim}D\n")
                     new_block_counter+=1
-                    if(new_block_counter == len(sub_grid_name)): f.write(f"END_BLOCK_DATAGRID_{block_dim}")
+                    if(new_block_counter == len(sub_grid_name)or new_block_counter-1==0): f.write(f"END_BLOCK_DATAGRID_{block_dim}D")
 
     @staticmethod
     def read_xsf(filename):
         xsf_file = YamboXsf()
 
         with open(filename, 'r') as f:
-            lines = f.readlines()
+            lines = [line.lstrip() for line in f.readlines()]
 
         #extract the dimension
         try: 
@@ -112,14 +115,19 @@ class YamboXsf():
         for line in lines[cell_parameters_start:cell_parameters_end]:
             cell_parameters.append([float(x) for x in line.split()])
         #Needed only if the convention cell is specified
-        if(lines.index("CONVVEC\n")):
-            xsf_file.set_lconventional(True)
-            cell_parameters_conv=[]
-            cell_parameters_conv_start = lines.index("CONVVEC\n")+1
-            cell_parameters_conv_end = cell_parameters_conv_start+3
-            for line in lines[cell_parameters_conv_start:cell_parameters_conv_end]:
-                cell_parameters_conv.append([float(x) for x in line.split()])    
-            xsf_file.set_cell_parameters_conv(cell_parameters_conv)
+        try:
+            if(lines.index("CONVVEC\n")):
+                xsf_file.set_lconventional(True)
+                cell_parameters_conv=[]
+                cell_parameters_conv_start = lines.index("CONVVEC\n")+1
+                cell_parameters_conv_end = cell_parameters_conv_start+3
+                for line in lines[cell_parameters_conv_start:cell_parameters_conv_end]:
+                    cell_parameters_conv.append([float(x) for x in line.split()])    
+                xsf_file.set_cell_parameters_conv(cell_parameters_conv)
+            else:
+                ValueError("conventional cell not present or not found.")
+        except ValueError as e:       
+            print(e) 
         # primitive cell parameters 
         xsf_file.set_cell_parameters(cell_parameters)
         # Extract atom positions
@@ -139,30 +147,37 @@ class YamboXsf():
             if line.startswith(f"BEGIN_BLOCK_DATAGRID_"):
                 block_start_idx.append(i)  
                 block_dim.append(int(re.findall('[0-9]',line)[0])) 
+                print(block_start_idx, block_dim)
 
-        for start_idx in block_start_idx:
-            block_end_idx = lines.index(f"END_BLOCK_DATAGRID_{block_dim[start_idx]}", start_idx) + 1
+        for istart_idx, start_idx in enumerate(block_start_idx):
+            print(istart_idx,start_idx)
+            block_end_idx = lines.index(f"END_BLOCK_DATAGRID_{block_dim[istart_idx]}D\n", start_idx) + 1
             block_lines = lines[start_idx:block_end_idx]
-            
-            subblock_start_idx = [i for i,line in enumerate(lines) if line.startswith(f"BEGIN_DATAGRID_{block_dim[start_idx]}")]
+            #print(block_lines)
+
+            subblock_start_idx = [i for i,line in enumerate(lines) if line.startswith(f"BEGIN_DATAGRID_{block_dim[istart_idx]}D\n")]
+            print(subblock_start_idx)
             grid_name = block_lines[1].strip()
             for sub_start_idx in subblock_start_idx:
-                sub_block_end_idx = lines.index(f"END_DATAGRID_{block_dim[start_idx]}", sub_start_idx)+1
+                sub_data_vectors=[]
+                sub_block_end_idx = lines.index(f"END_DATAGRID_{block_dim[istart_idx]}D\n", sub_start_idx)+1
                 sub_block_lines = lines[sub_start_idx:sub_block_end_idx]
                 sub_block_name = re.split('_',sub_block_lines[0]) # extract name of the subblock
                 sub_data_size = [int(x) for x in sub_block_lines[1].split()]
                 sub_data_num = len(sub_data_size) # number of subdata for each block
                 sub_data_origin = sub_block_lines[2].split()
-                sub_data_vectors = [float(x) for x in sub_block_lines[3:3+len(sub_data_num)]]
+                for j in range(3, 3+ sub_data_num):
+                    sub_data_vectors.append([float(x) for x in sub_block_lines[j].split()])
                 sub_data_values = []
-                for j in range(3+sub_data_num,len(sub_block_lines)):
-                    sub_data_values.extend(float(x) for x in sub_block_lines[j].split())
-                sub_data_array = np.array(sub_data_values).reshape(sub_data_size)
-                if(block_dim[start_idx]==2): 
+                for j in range(3+sub_data_num,len(sub_block_lines)-1): # -1 becuase I do not want to include the last line
+                    sub_data_values.append([float(x) for x in sub_block_lines[j].split()])
+                sub_data_array = np.array(sub_data_values).reshape(np.flip(sub_data_size)) #reverse ordering cause fortran
+                if(block_dim[istart_idx]==2): 
                     na = np.newaxis
                     sub_data_array = sub_data_array[:,:,na]
-                xsf_file.add_grid_data(grid_name,block_dim=block_dim,sub_grid_name=sub_block_name, sub_grid_dim=block_dim[start_idx], sub_grid_origin=sub_data_origin \
-                ,sub_grid_vectors=sub_data_vectors, data_array=sub_data_array)
+                xsf_file.add_grid_data(grid_name,block_dim=block_dim[istart_idx],sub_grid_name=sub_block_name, sub_grid_size=sub_data_size, \
+                    sub_grid_dim=block_dim[istart_idx], sub_grid_origin=sub_data_origin, \
+                    sub_grid_vectors=np.array(sub_data_vectors), data_array=sub_data_array)
 
         return xsf_file
     #instance of xsf file from lattice db class
