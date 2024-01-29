0d7515d1461d8bfeb06d53774ddc435b6d14b846
Fulvio Paleari <fulvio.paleari90@gmail.com>
2024-01-29 16:18:55 +0100

    Switched back to setuptools as backend for building the distribution

--- yambocommandline/__init__.py
+++ yambocommandline/__init__.py
@@ -8,12 +8,12 @@ Scripts to be run from the command line using the 'yambopy' executable
  -outside recipes: groups of functions to perform a single command-line operation
 """
 
-import yambocommandline.commands.recipes
-import yambocommandline.commands.generate_save
-import yambocommandline.commands.generate_bands
-import yambocommandline.commands.band_plots
-import yambocommandline.commands.gkkp
-import yambocommandline.commands.update_serial
-import yambocommandline.commands.get_phq_input
-import yambocommandline.commands.gw_subspace
-import yambocommandline.commands.convert_RL_to_Ry
+from yambocommandline.commands.recipes import *
+from yambocommandline.commands.generate_save import *
+from yambocommandline.commands.generate_bands import *
+from yambocommandline.commands.band_plots import *
+from yambocommandline.commands.gkkp import *
+from yambocommandline.commands.update_serial import *
+from yambocommandline.commands.get_phq_input import *
+from yambocommandline.commands.gw_subspace import *
+from yambocommandline.commands.convert_RL_to_Ry import *
