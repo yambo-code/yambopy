3bed494d252294ccdf4b294518852099c42e3bcf
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2024-01-31 15:54:42 +0100

    Fixing bug

--- yambopy/nl/harmonic_analysis.py
+++ yambopy/nl/harmonic_analysis.py
@@ -171,18 +171,19 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_P_fitted=False):
             for i_d in range(3):
                 for i_order1 in range(-X_order,X_order+1):
                     P_fitted[i_f,i_d,:]+=X_effective[i_order+X_order,i_f,i_d]*np.exp(-1j*i_order*freqs[i_f]* time[:],dtype=np.cdouble)
-        for i_f in range(n_runs):
-        values2=np.c_[time.real/fs2aut]
-        values2=np.append(values2,np.c_[P_fitted[i_f,0,:].real],axis=1)
-        values2=np.append(values2,np.c_[P_fitted[i_f,1,:].real],axis=1)
-        values2=np.append(values2,np.c_[P_fitted[i_f,2,:].real],axis=1)
-        output_file2='o.Pol_fitted_'+str(i_f+1)
+        # Write on disk
         header2="[fs]            "
         header2+="Px     "
         header2+="Py     "
         header2+="Pz     "
         footer2='Time dependent polarization reconstructed from Fourier coefficients'
-        np.savetxt(output_file2,values2,header=header2,delimiter=' ',footer=footer2)
+        for i_f in range(n_runs):
+            values2=np.c_[time.real/fs2aut]
+            values2=np.append(values2,np.c_[P_fitted[i_f,0,:].real],axis=1)
+            values2=np.append(values2,np.c_[P_fitted[i_f,1,:].real],axis=1)
+            values2=np.append(values2,np.c_[P_fitted[i_f,2,:].real],axis=1)
+            output_file2='o.Pol_fitted_'+str(i_f+1)
+            np.savetxt(output_file2,values2,header=header2,delimiter=' ',footer=footer2)
 
 
     # Print the result
