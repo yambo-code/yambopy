55b3226b1b09da02d103aef08e06dd806d0e4336
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2024-02-01 12:42:41 +0100

    Fixed reading old DBS

--- yambopy/dbs/nldb.py
+++ yambopy/dbs/nldb.py
@@ -68,11 +68,16 @@ class YamboNLDB(object):
         self.NE_steps       = database.variables['NE_steps'][0].astype('int')
         self.RT_step        = database.variables['RT_step'][0].astype(np.double)
         self.n_frequencies  = database.variables['n_frequencies'][0].astype('int')
+
         try:
             self.n_angles       = database.variables['n_angles'][0].astype('int')
         except:
             self.n_angles   = 0
-        self.NL_initial_versor = database.variables['NL_initial_versor'][:].astype(np.double)
+        try:
+            self.NL_initial_versor = database.variables['NL_initial_versor'][:].astype(np.double)
+        except:
+            self.NL_initial_versor = [0.0, 0.0, 0.0] 
+
         self.NL_damping     = database.variables['NL_damping'][0].astype(np.double)
         self.RT_bands       = database.variables['RT_bands'][:].astype('int')
         self.NL_er          = database.variables['NL_er'][:].astype(np.double)
