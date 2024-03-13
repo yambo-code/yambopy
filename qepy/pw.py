e237bcaa9caabee2b75b7fbeefce8da97dc6d4dc
stevenbos123 <steventhijsbos98@gmail.com>
2024-03-13 15:38:39 +0100

    fixed: pw readin bug scientific notation for d+0? was not matched, also  /%end does not work for all qe versions, tested and fixed

--- qepy/pw.py
+++ qepy/pw.py
@@ -688,7 +688,7 @@ class PwIn(object):
         """
         Save the variables specified in each of the groups on the structure
         """
-        group_regexp = '([a-zA-Z_0-9_\(\)]+)(?:\s+)?=(?:\s+)?([a-zA-Z/\'"0-9_.-]+)'
+        group_regexp = '([a-zA-Z_0-9\(\)]+)\s*=\s*([\d.-]+d[+\-]\d+|[a-zA-Z/\'"0-9_.-]+)'   ## also match scientific notation
         for file_slice in self.slicefile(name):
             for keyword, value in re.findall(group_regexp,file_slice):
                 group[keyword.strip()]=value.strip()
@@ -698,7 +698,7 @@ class PwIn(object):
             string='&%s\n' % keyword
             for keyword in sorted(group): # Py2/3 discrepancy in keyword order
                 string += "%20s = %s\n" % (keyword, group[keyword])
-            string += "/&end"
+            string += "/"    ### /%&end does not work for all qe versions
             return string
         else:
             return ''
