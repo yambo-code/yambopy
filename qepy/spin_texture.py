e4b861b05afb963c43c1dc382a41b80882bec3d1
jorgecvm <cervantes.jorge9@gmail.com>
2024-12-18 12:07:26 +0100

    New feature: Spin texture

---
+++ qepy/spin_texture.py
@@ -0,0 +1,21 @@
+# 
+# License-Identifier: GPL
+#
+# Copyright (C) 2024 The Yambo Team
+#
+# Authors: JC-V
+# First verion by JC-V (2024)
+#
+# This file is part of the yambopy project
+#
+#
+
+from qepy import *
+from yambopy import *
+
+
+class Spin_texture():
+
+    def __init__(self):
+
+        return print("Done!")
