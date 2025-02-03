481d8199d2dc82deea7180e79dd8ef5a27b7cc28
Murali <muralidharrsvm7@gmail.com>
2025-02-03 22:46:53 +0100

    rewrite wfcdb

--- yambopy/dbs/wfdb.py
+++ yambopy/dbs/wfdb.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 import scipy.fft
 from yambopy.io.cubetools import write_cube
 from yambopy.dbs.latticedb import YamboLatticeDB
-
+from scipy.spatial import KDTree
 
 class YamboWFDB():
     """
@@ -61,6 +61,10 @@ class YamboWFDB():
             lat_vec = ns_db1['LATTICE_VECTORS'][...].data
             self.lat_vec = lat_vec # ai = lat_vec[:,i] 
             lat_param = ns_db1['LATTICE_PARAMETER'][...].data
+            #
+            ## kpoints in iBZ (crystal units)
+            self.kpts_iBZ = self.ydb.iku_kpoints/lat_param[None,:]
+            self.kpts_iBZ = self.kpts_iBZ @ lat_vec
             #
             G_vec = ns_db1['G-VECTORS'][...].data.T # Gvec database
             #
@@ -227,7 +231,6 @@ class YamboWFDB():
         ## Incase, the user wants to provide something, it must be greater than default box
         ### default box = self.fft_box
 
-        cel_vol = abs(np.linalg.det(self.lat_vec))
         assert len(grid) == 0 or len(grid) == 3, "grid must be empty list or list of 3 integers"
         assert ik >= min(self.nkpts_range) and ik <= max(self.nkpts_range), \
             "Invalid kpoint idx. Provided out side the range %d - %d" \
@@ -249,28 +252,25 @@ class YamboWFDB():
         wfc_tmp = self.wf[ikk][:,ibb,:,:self.ngvecs[ik-1]]
         gvec_tmp = self.gvecs[ikk,:self.ngvecs[ik-1],:]
         #
-        tmp_wfc = np.zeros((self.nspin,self.nspinor, grid[0], grid[1], grid[2]),dtype=wfc_tmp.dtype)
-        #
-        Nx_vals = np.where(gvec_tmp[:,0] >= 0, gvec_tmp[:,0], gvec_tmp[:,0] + grid[0])
-        Ny_vals = np.where(gvec_tmp[:,1] >= 0, gvec_tmp[:,1], gvec_tmp[:,1] + grid[1])
-        Nz_vals = np.where(gvec_tmp[:,2] >= 0, gvec_tmp[:,2], gvec_tmp[:,2] + grid[2])
+        return self.to_real_space(wfc_tmp, gvec_tmp, grid = grid)
         
-        assert np.min(Nx_vals) >=0 and (np.min(Ny_vals) >=0 and np.min(Nz_vals)) >=0, "Wrong fft indices"
-        tmp_wfc[:,:,Nx_vals, Ny_vals,Nz_vals] = wfc_tmp
-        return scipy.fft.ifftn(tmp_wfc,norm="forward",axes=(2,3,4))/np.sqrt(cel_vol)
-
     def write2cube(self, ik, ib, grid=[]):
         wfc_r = self.wfcG2r(ik, ib, grid=grid)
         wfc_r = np.sum(np.abs(wfc_r)**2,axis=(1))
         wfc_r = wfc_r/wfc_r.max()
         for ispin in range(self.nspin):
-            filename = 'wfc_k%d_spin%d.cube' %(ik,ispin+1)
+            filename = 'wfc_k%d_bnd_%d_spin%d.cube' %(ik, ib, ispin+1)
             write_cube(filename, wfc_r[ispin], self.lat_vec, self.ydb.car_atomic_positions, \
                        self.ydb.atomic_numbers, header='Real space electronic wavefunction')
 
+    def get_wfc_kpt(self, ik):
+        # return kpoint of ik wfc in crystal units
+        assert ik >= min(self.nkpts_range) and ik <= max(self.nkpts_range), \
+            "Invalid kpoint idx. Provided out side the range %d - %d" \
+            %(min(self.nkpts_range), max(self.nkpts_range))
+        return self.kpts_iBZ[ik]
 
-
-    def rotate_wfc(self, ik, time_rev, sym_mat, frac_vec=np.array([0,0,0]), space = 'g'):
+    def rotate_wfc(self, ik, time_rev, sym_mat, frac_vec=np.array([0,0,0])):
         ## Applies a symmetry operaton on wavefuntion.
         ### Given a symmetry operation x -> sym_mat * x + frac_vec 
         ### get the rotated wfc i.e U(R) * psi
@@ -284,11 +284,109 @@ class YamboWFDB():
         ## time_rev == true if sym_mat is a time-reversal symmetry. 
         ## In yambo the standard convertion of representing time reversal is to use -R, -tau
         ##
+        ## returns [Rotated kpoints, wfc, gvecs]
         wfc_k, gvecs_k = self.get_wf(ik)
         
+        su_mat = np.eye(2)
+        if self.nspinor == 1: su_mat = 1
+        else : su_mat = su2_mat(sym_mat,time_rev).astype(wfc_k.dtype)
         ### rotate gvecs_k
+        sym_red = np.linalg.inv(self.lat_vec) @ sym_mat.T @ self.lat_vec
+        sym_red = np.rint(sym_red).astype(int)
+        gvec_rot = gvecs_k@sym_red
+        
+        if self.nspinor == 2:
+            wfc_rot = np.einsum('ij,sbjg->sbig',su_mat,wfc_k,optimize=True)
+
+        ## add phase due to fractional translation
+        Rkvec = sym_red.T@self.kpts_iBZ[ik]
+        tau_frac = lat@frac_vec
+
+        kphase = np.exp( -1j * 2*np.pi * np.dot(Rkvec, tau_frac))
+        gphase = kphase*np.exp( -1j * 2*np.pi * (gvec_rot@tau_frac))
+
+        wfc_rot *= gphase[None,None,None,:]
+        if time_rev: wfc_rot = wfc_rot.conj()
+
+        return [Rkvec, wfc_rot,gvec_rot]
+
+    
+    def to_real_space(self, wfc_tmp, gvec_tmp, grid = []):
+        #
+        # COnvert a wfc from Gspace to real space 
+        # wfc_tmp C_G
+        # gvec_tmp miller indices
+        # grid fft grid (if emtpy, default will used)
+        #
+        if len(grid) == 0: grid = self.fft_box
+        cel_vol = abs(np.linalg.det(self.lat_vec))
+        #
+        tmp_wfc = np.zeros((self.nspin,self.nspinor, grid[0], grid[1], grid[2]),dtype=wfc_tmp.dtype)
+        #
+        Nx_vals = np.where(gvec_tmp[:,0] >= 0, gvec_tmp[:,0], gvec_tmp[:,0] + grid[0])
+        Ny_vals = np.where(gvec_tmp[:,1] >= 0, gvec_tmp[:,1], gvec_tmp[:,1] + grid[1])
+        Nz_vals = np.where(gvec_tmp[:,2] >= 0, gvec_tmp[:,2], gvec_tmp[:,2] + grid[2])
+
+        assert np.min(Nx_vals) >=0 and (np.min(Ny_vals) >=0 and np.min(Nz_vals)) >=0, "Wrong fft indices"
+        tmp_wfc[:,:,Nx_vals, Ny_vals,Nz_vals] = wfc_tmp
+        return scipy.fft.ifftn(tmp_wfc,norm="forward",axes=(2,3,4))/np.sqrt(cel_vol)
 
 
+
+def wfc_inner_product(k_bra, wfc_bra, gvec_bra, k_ket, wfc_ket, gvec_ket):
+    ## k1, k2, are kpoint indices in crystal coordinates
+    ## wfc1 and wfc2 are wfc coeffiencts (nspin, nbnd, nspinor, ng)
+    ## gvec1 and gvec2 are miller indices (ng,3)
+    ## < k2 | k1>
+    #
+    assert wfc_ket.shape[:3] == wfc_bra.shape[:3], "Inconsistant wfcs"
+    #
+    nspin, nbnd, nspinor = wfc_ket.shape[:3]
+    kdiff = k_bra-k_ket
+    G0 = np.rint(kdiff).astype(int)
+    kdiff = kdiff-G0
+    ## crystal momentum mismatch delta_{k,k'}
+    if np.max(np.abs(kdiff)) > 1e-5:
+        return np.zeros((nspin, nbnd, nbnd),dtype=wfc_ket.dtype)
+    ket_Gtree = KDTree(gvec_ket)
+    gbra_shift = gvec_bra + G0[None,:]
+    dd, ii = ket_Gtree.query(gbra_shift, k=1)
+    wfc_bra_tmp = np.zero(wfc_ket.shape,dtype=wfc_ket.dtype)
+    bra_idx = ii[dd < 1e-6]
+    wfc_bra_tmp[:,:,:,bra_idx] = wfc_bra[...,dd<1e-6].conj()
+    return np.einsum('sixg,sjxg->sij',wfc_bra_tmp,wfc_ket,optimize=True)
+    
+
+
+def su2_mat(symm_mat,time_rev1):
+    # Computes the Spinor rotation matrix
+    if np.isclose(np.abs(np.trace(symm_mat)),3,atol=1e-03):
+        su_mat = np.eye(2)
+    else :
+        sym_temp = symm_mat/np.linalg.det(symm_mat)
+        
+        if abs(sym_temp[2,0] +1) < 10**-5 :
+            alpha = 0
+            beta = -np.pi/2
+            delta = np.arctan2(sym_temp[0,1],sym_temp[0,2])
+        elif abs(sym_temp[2,0] - 1) < 10**-5:
+            alpha = 0
+            beta = np.pi/2
+            delta = np.arctan2(-sym_temp[0,1],-sym_temp[0,2])
+        else :
+            beta = np.arcsin(sym_temp[2,0])
+            alpha = np.arctan2(sym_temp[1,0]/np.cos(beta),sym_temp[0,0]/np.cos(beta))
+            delta = np.arctan2(sym_temp[2,1]/np.cos(beta),sym_temp[2,2]/np.cos(beta))
+        # print(180/np.pi*alpha,180/np.pi*beta,180/np.pi*delta)
+        alpha = alpha/2 ; beta = -beta/2 ; delta = delta/2 ;
+        spin_RX_delta=sigma_0*np.cos(delta)-1j*sigma_x*np.sin(delta)
+        spin_RY_beta =sigma_0*np.cos(beta) -1j*sigma_y*np.sin(beta)
+        spin_RZ_alpha=sigma_0*np.cos(alpha)-1j*sigma_z*np.sin(alpha)
+        su_mat = spin_RZ_alpha@spin_RY_beta@spin_RX_delta
+    if time_rev1:
+        ## -ve sign because of yambo does it wrong
+        su_mat = -np.array([[0, -1], [1, 0]])@su_mat
+    return su_mat
 
 
 if __name__ == "__main__":
