5c60c092ff48ecb307ee2d90be3e716d95c6170c
Murali <muralidharrsvm7@gmail.com>
2025-02-03 14:01:12 +0100

    rewrite wfcdb

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git init
 git add -A
 git commit -m "rewrite wfcdb"
-git branch -M master
+git branch -M bug_fixes_ypy
 #git remote add origin git@github.com:muralidhar-nalabothula/elboC.git
-git push -u origin master
+git push -u origin bug_fixes_ypy
