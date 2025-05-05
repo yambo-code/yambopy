Fixed units for shift current

yambopy/nl/harmonic_analysis.py
@@ -5,14 +5,14 @@
 # Calculate linear response from real-time calculations (yambo_nl)
 #
 import numpy as np
-from yambopy.units import ha2ev,fs2aut, SVCMm12VMm1,AU2VMm1
+from yambopy.units import ha2ev,fs2aut, SVCMm12VMm1,AU2VMm1, Junit, EFunit
 from yambopy.nl.external_efield import Divide_by_the_Field
 from tqdm import tqdm
 import scipy.linalg
 import sys
 import os
 
-def Coefficients_Inversion(NW,NX,P,W,T_period,T_range,T_step,efield,INV_MODE):
+def Coefficients_Inversion(NW,NX,P,W,T_period,T_range,T_step,efield,INV_MODE, tol=1e-8):
     """
     Compute coefficients inversion using various inversion modes.
     see Sec. III in PRB 88, 235113 (2013) 
@@ -104,7 +104,7 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
         prn_Xhi (bool): Print susceptibilities if True.
 
     Returns:
-        tuple: Frequencies, susceptibilities and conducibilities if prn_Xhi is False.
+        tuple: Frequencies, susceptibilities and conductibilities if prn_Xhi is False.
     """
     # Time series
     time = nldb.IO_TIME_points
@@ -136,7 +136,7 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
         sys.exit(0)
 
     
-    print(f"Current is {'present' if l_eval_current else 'not present'}: conducibilities will {'not' if l_eval_current else ''} be calculated")
+    print(f"Current is {'present' if l_eval_current else 'not present'}: conductibilities will {'not' if l_eval_current else ''} be calculated")
     print(f"Number of runs: {n_runs}")
 
     #Max and minimun frequencies
@@ -167,7 +167,7 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
     # Current response
     if l_eval_current:
         Sigma_effective = np.zeros((X_order + 1, n_runs, 3), dtype=np.cdouble)
-        Conducibility = np.zeros((X_order + 1, n_runs, 3), dtype=np.cdouble)
+        Conductibility = np.zeros((X_order + 1, n_runs, 3), dtype=np.cdouble)
         SamplingJ = np.zeros((M_size, 2, n_runs, 3), dtype=np.double)
 
     # Generate multiples of each frequency
@@ -208,15 +208,15 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             if i_order == 1:
                 Susceptibility[i_order, i_f, 0] = 4.0 * np.pi * np.dot(efield['versor'], X_effective[i_order, i_f, :])
                 if l_eval_current:
-                    Conducibility[i_order,i_f,0]= 4.0*np.pi*np.dot(efield['versor'][:],Sigma_effective[i_order,i_f,:])
+                    Conductibility[i_order,i_f,0]= 4.0*np.pi*np.dot(efield['versor'][:],Sigma_effective[i_order,i_f,:])
             else:
                 Susceptibility[i_order, i_f, :] = X_effective[i_order, i_f, :]
                 if l_eval_current:
-                    Conducibility[i_order,i_f,:] = Sigma_effective[i_order, i_f, :]
+                    Conductibility[i_order,i_f,:] = Sigma_effective[i_order, i_f, :]
 
             Susceptibility[i_order, i_f, :] *= Divide_by_the_Field(nldb.Efield[i_f], i_order)
             if l_eval_current:
-                Conducibility[i_order,i_f,:] *=Divide_by_the_Field(nldb.Efield[i_f],i_order)
+                Conductibility[i_order,i_f,:] *=Divide_by_the_Field(nldb.Efield[i_f],i_order)
 
 
     prefix = f'-{nldb.calc}' if nldb.calc != 'SAVE' else ''
@@ -255,11 +255,26 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
                 np.savetxt(output_file, values, header="[fs] Jx Jy Jz", delimiter=' ', footer="Reconstructed current")
 
     #Units of measure rescaling
+    '''
+    Comments: 
+    i_order is the order of harmonics, i.e. omega^{i_order}. Previous code thought i_order to be the same as the order of Electric field.
+    This leads to incorrect shift current conductivity.
+    Suppose we have a monochromatic light E(t) ~ cos(ometa t).
+    Shift current is zero-th order of harmonics, but second order of electric field. 
+    J(0 x omega) ~ sigma^(2) E(omega) E(-omega)
+    J(omega) ~ sigma^(1) E(omega) + sigma^(3) E(omega) E(omega) E(-omega)
+    J(2 omega) ~ sigma^(2) E(omega) E(omega)
+    etc...
+    '''
     for i_order in range(X_order+1):
-        Susceptibility[i_order,:,:]*=get_Unit_of_Measure(i_order)
+        Susceptibility[i_order,:,:]*=get_Unit_of_Measure(i_order) # Mao: not clear if this is correct
         if l_eval_current:
-            Conducibility[i_order,:,:]*=get_Unit_of_Measure(i_order)
-    
+        #     Conductibility[i_order,:,:]*=get_Unit_of_Measure(i_order)
+            if i_order == 0: # shift current is special case of second order
+                Conductibility[i_order, :, :] *= Junit/(EFunit**2) 
+            else:
+                Conductibility[i_order, :, :] *= Junit/(EFunit**i_order) # i_order is the order of harmonics omega^{i_order}, not order of E-field.
+
     # Write final results
     if prn_Xhi:
         print("Write final results: xhi^1, xhi^2, xhi^3, etc...")
@@ -275,13 +290,13 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             for i_order in range(X_order + 1):
                 output_file = f'o{prefix}.YamboPy-Sigma_probe_order_{i_order}'
                 header = "[eV] " + " ".join([f"S/Im(z){i_order} S/Re(z){i_order}" for _ in range(3)])
-                values = np.column_stack((freqs * ha2ev, Conducibility[i_order, :, 0].imag, Conducibility[i_order, :, 0].real,
-                                      Conducibility[i_order, :, 1].imag, Conducibility[i_order, :, 1].real,
-                                      Conducibility[i_order, :, 2].imag, Conducibility[i_order, :, 2].real))
+                values = np.column_stack((freqs * ha2ev, Conductibility[i_order, :, 0].imag, Conductibility[i_order, :, 0].real,
+                                      Conductibility[i_order, :, 1].imag, Conductibility[i_order, :, 1].real,
+                                      Conductibility[i_order, :, 2].imag, Conductibility[i_order, :, 2].real))
                 np.savetxt(output_file, values, header=header, delimiter=' ', footer="Current Harmonic analysis results")
 
     else:
-        return (freqs, Susceptibility, Conducibility) if l_eval_current else (freqs, Susceptibility)
+        return (freqs, Susceptibility, Conductibility) if l_eval_current else (freqs, Susceptibility)
 
 
 def get_Unit_of_Measure(i_order):