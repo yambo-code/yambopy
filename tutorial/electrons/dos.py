42a1070881edf6f532131140362ce6042d317659
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-05-16 18:28:08 +0200

    Added dos

--- tutorial/electrons/dos.py
+++ tutorial/electrons/dos.py
@@ -3,3 +3,13 @@
 #
 from qepy import *
 from yambopy import *
+import matplotlib.pyplot as plt
+
+save_folder='./SAVE' 
+yel = YamboElectronsDB.from_db_file(folder=save_folder)
+dos=yel.getDOS()
+
+fig = plt.figure(figsize=(6,4))
+ax.set_xlabel('$Energy[eV]$')
+ax.set_ylabel('DOS$')
+plt.
