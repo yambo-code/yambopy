ab1e97524f0dd5289f0419dfa9b7aa78c89611e4
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-08-16 12:39:23 +0200

    More stuff

--- tutorial/pert2yambo/test_pert2yambo.py
+++ tutorial/pert2yambo/test_pert2yambo.py
@@ -23,6 +23,22 @@ yamlfile_h = "./cdyna-hole/gaas_dynamics-run.yml"
 #Debug=False
 Debug=True
 
+
+def generate_grid(grid):
+    dx=1.0/float(grid[0])
+    dy=1.0/float(grid[1])
+    dz=1.0/float(grid[2])
+    nkpt=np.prod(grid)
+    k_grid=zeros([nkpt,3],float)
+    ic=0
+    for kx in range(0, grid[0]):
+        for ky in range(0, grid[1]):
+            for kz in range(0, grid[2]):
+                k_grid[ic,:]=[float(kx)*dx,float(ky)*dy,float(kz)*dz]
+                ic=ic+1
+    return k_grid
+
+
 # Return the integer distance for periodic grids
 # defined by kgrid
 def periodic_dist(ikpt1,ikpt2,kgrid):
@@ -42,6 +58,17 @@ ylat = YamboLatticeDB.from_db_file(filename=save_path+'/SAVE/ns.db1')
 y_k_grid=ylat.k_grid
 p_k_grid=dynoccups.kpts_grid
 
+### Generate fake grids to check the code ##############
+if Debug:
+    y_grid=np.array([4,1,1])
+    p_grid=np.array([12,1,1])
+    
+    y_k_grid=y_grid
+    p_k_grid=p_grid
+
+#######################################################
+
+
 print("Perturbo k-grid ",p_k_grid)
 print("Yambo k-grid ",y_k_grid)
 
@@ -58,10 +85,17 @@ if(any(grid_ratio%2 ==0)):
    sys.exit(0)
 
 dynoccups.read_pert_num_kpts()
-n_kpt_pert=dynoccups.num_pert_kpts
+if Debug:
+    n_kpt_pert=np.prod(p_k_grid)
+else:
+    n_kpt_pert=dynoccups.num_pert_kpts
 print("Number of k-points in perturbo : ",n_kpt_pert)
 
-pert_kpts=dynoccups.read_perturbo_kpts()
+if Debug:
+    pert_kpts=generate_grid(p_k_grid)
+else:
+    pert_kpts=dynoccups.read_perturbo_kpts()
+
 
 pert_kpts=make_kpositive(pert_kpts.tolist())
 small_q=np.zeros(3,dtype=float)
