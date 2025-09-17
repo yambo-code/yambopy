10c526aecb857e13815e59c55a7309ff8ac5b4d7
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-09-17 11:01:53 +0200

    More stuff

--- yambopy/thermal/map_phonons.py
+++ yambopy/thermal/map_phonons.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 #
 from qepy.supercell import Supercell
+import numpy as np
 
-def Map_Phonons(self,qe_input, qe_dyn, R, supercell_name=None, dynmat_name=None):           # R is the supercell
+def Map_Phonons(qe_input, qe_dyn, R, sc_fname=None, dyn_fname=None, debug=None):           # R is the supercell
     
     print(" \n\n\n * * * Map phonons in a supercell * * *\n")
     print(" This code works only without symmetries!!! \n")
+
+    #Check and map phonons
+    if qe_dyn.nqpoints == np.prod(R):
+        print("Error: number of q-points not compatible with supercell ") 
+    
+
 
     sc=Supercell(qe_input)
     sc.d_sup(R)
 
     #write supercell to file
-    sc.qe_d.write(qe_d.filename+'_sc')
+    sc.qe_d.write(sc.qe_d.filename+'_sc')
 
-    #Check and map phonons
