d4bb446f54cf366c1eaacddf4e6b3ea4a62bca2b
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-09-17 08:22:39 +0200

    Work in progress

---
+++ yambopy/thermal/map_phonons.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#
+from qepy.supercell import Supercell
+
+def map_phonons(self,qe_input, qe_dyn, R, supercell_name=None, dynmat_name=None):           # R is the supercell
+    
+    print(" \n\n\n * * * Map phonons in a supercell * * *\n")
+    print(" This code works only without symmetries!!! \n")
+
+    sc=Supercell(qe_input)
+    sc.d_sup(R)
+
+    #write supercell to file
+    sc.qe_d.write(qe_d.filename+'_sc')
+
+    #Check and map phonons
