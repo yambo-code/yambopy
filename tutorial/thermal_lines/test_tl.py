aa5608b356bc3dee1093e992666e100ab509de00
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-09-24 17:16:12 +0200

    Added new file

--- tutorial/thermal_lines/test_tl.py
+++
@@ -1,8 +0,0 @@
-from qepy import *
-
-uc="diamond.scf.in"
-eigv_filename ="matdyn.modes"
-qe = PwIn.from_file(uc)
-qe_dyn = Matdyn.from_modes_file(filename=eigv_filename)
-
-single_mode_thermal_line(qe_input, qe_dyn, modes=[3,4,5])
