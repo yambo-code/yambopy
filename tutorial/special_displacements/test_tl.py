459f411d1d0855079ec924c29d4a1afb2abe0cd3
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-10-28 16:11:23 +0100

    Fixed generation ZG

--- tutorial/special_displacements/test_tl.py
+++ tutorial/special_displacements/test_tl.py
@@ -5,4 +5,4 @@ eigv_filename ="matdyn.modes"
 qe = PwIn.from_file(uc)
 qe_dyn = Matdyn.from_modes_file(filename=eigv_filename)
 
-single_mode_thermal_line(qe_input, qe_dyn, modes=[3,4,5])
+generate_ZG_conf(qe, qe_dyn, modes=[3,4,5])
